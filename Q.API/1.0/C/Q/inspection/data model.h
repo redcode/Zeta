1ee9b9ea8b4105df6d78d2dfc27922af32b5e122
redcode <manuel@redcodesoftware.com>
2013-08-09 05:07:46 +0200

    C data model autodetection in Windows 32/64 bit

--- Q.API/1.0/C/Q/inspection/data model.h
+++ Q.API/1.0/C/Q/inspection/data model.h
@@ -92,18 +92,17 @@ Released under the terms of the GNU General Public License v2. */
 
 #endif
 
-#define Q_DATA_MODEL_HEADER \
-	<Q/formats/data model/Q_DATA_MODEL_NAME.h>
+#define Q_DATA_MODEL_HEADER		<Q/formats/data model/Q_DATA_MODEL_NAME.h>
 
 #include Q_DATA_MODEL_HEADER
 #include <Q/macros/preprocessor.h>
 #include <Q/keys/data model.h>
 
-#define Q_DATA_MODEL				Q_JOIN_2(Q_DATA_MODEL_, Q_DATA_MODEL_NAME)
-#define Q_DATA_MODEL_STRING			Q_JOIN_2(Q_DATA_MODEL_STRING_, Q_DATA_MODEL_NAME)
-#define Q_DATA_MODEL_BITS(type)			Q_JOIN_4(Q_, Q_DATA_MODEL_NAME, _BITS_, type)
-#define Q_DATA_MODEL_TYPE(type)			Q_JOIN_4(Q_, Q_DATA_MODEL_NAME, _TYPE_, type)
-#define Q_DATA_MODEL_BASE_TYPE(type)		Q_JOIN_4(Q_, Q_DATA_MODEL_NAME, _BASE_TYPE_, type)
-#define Q_DATA_MODEL_LITERAL(type)		Q_JOIN_4(Q_, Q_DATA_MODEL_NAME, _LITERAL_, type)
+#define Q_DATA_MODEL			Q_JOIN_2(Q_DATA_MODEL_, Q_DATA_MODEL_NAME)
+#define Q_DATA_MODEL_STRING		Q_JOIN_2(Q_DATA_MODEL_STRING_, Q_DATA_MODEL_NAME)
+#define Q_DATA_MODEL_BITS(type)		Q_JOIN_4(Q_, Q_DATA_MODEL_NAME, _BITS_, type)
+#define Q_DATA_MODEL_TYPE(type)		Q_JOIN_4(Q_, Q_DATA_MODEL_NAME, _TYPE_, type)
+#define Q_DATA_MODEL_BASE_TYPE(type)	Q_JOIN_4(Q_, Q_DATA_MODEL_NAME, _BASE_TYPE_, type)
+#define Q_DATA_MODEL_LITERAL(type)	Q_JOIN_4(Q_, Q_DATA_MODEL_NAME, _LITERAL_, type)
 
 #endif /* __Q_inspection_data_model_H__ */
