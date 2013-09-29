b768a172ad608a86055c4c2c2b13b954fd5f5a6e
redcode <manuel@redcodesoftware.com>
2013-09-30 01:03:26 +0200

    macros for generic function types

--- Q.API/1.0/C/Q/types/generic functions.h
+++ Q.API/1.0/C/Q/types/generic functions.h
@@ -13,155 +13,155 @@ Released under the terms of the GNU General Public License v3. */
 
 /* MARK: - General pourpose context access function types */
 
-typedef void	(* QDo)				(void* context);
-typedef void	(* QSwitch)			(void* context, qboolean state);
+typedef void	(* QDo)			    (void* context);
+typedef void	(* QSwitch)		    (void* context, qboolean state);
 
-typedef quint8	(* Q16BitAddressRead8Bit )	(void* context, quint16 address);
-typedef quint16	(* Q16BitAddressRead16Bit)	(void* context, quint16 address);
-typedef quint32 (* Q16BitAddressRead32Bit)	(void* context, quint16 address);
-typedef quint64 (* Q16BitAddressRead64Bit)	(void* context, quint16 address);
+typedef quint8	(* Q16BitAddressRead8Bit )  (void* context, quint16 address);
+typedef quint16 (* Q16BitAddressRead16Bit)  (void* context, quint16 address);
+typedef quint32 (* Q16BitAddressRead32Bit)  (void* context, quint16 address);
+typedef quint64 (* Q16BitAddressRead64Bit)  (void* context, quint16 address);
 
-typedef void	(* Q16BitAddressWrite8Bit )	(void* context, quint16 address, quint8  value);
-typedef void	(* Q16BitAddressWrite16Bit)	(void* context, quint16 address, quint16 value);
-typedef void	(* Q16BitAddressWrite32Bit)	(void* context, quint16 address, quint32 value);
-typedef void	(* Q16BitAddressWrite64Bit)	(void* context, quint16 address, quint64 value);
+typedef void	(* Q16BitAddressWrite8Bit ) (void* context, quint16 address, quint8  value);
+typedef void	(* Q16BitAddressWrite16Bit) (void* context, quint16 address, quint16 value);
+typedef void	(* Q16BitAddressWrite32Bit) (void* context, quint16 address, quint32 value);
+typedef void	(* Q16BitAddressWrite64Bit) (void* context, quint16 address, quint64 value);
 
-typedef quint8	(* Q32BitAddressRead8Bit )	(void* context, quint32 address);
-typedef quint16	(* Q32BitAddressRead16Bit)	(void* context, quint32 address);
-typedef quint32 (* Q32BitAddressRead32Bit)	(void* context, quint32 address);
-typedef quint64 (* Q32BitAddressRead64Bit)	(void* context, quint32 address);
+typedef quint8	(* Q32BitAddressRead8Bit )  (void* context, quint32 address);
+typedef quint16 (* Q32BitAddressRead16Bit)  (void* context, quint32 address);
+typedef quint32 (* Q32BitAddressRead32Bit)  (void* context, quint32 address);
+typedef quint64 (* Q32BitAddressRead64Bit)  (void* context, quint32 address);
 
-typedef void	(* Q32BitAddressWrite8Bit )	(void* context, quint32 address, quint8  value);
-typedef void	(* Q32BitAddressWrite16Bit)	(void* context, quint32 address, quint16 value);
-typedef void	(* Q32BitAddressWrite32Bit)	(void* context, quint32 address, quint32 value);
-typedef void	(* Q32BitAddressWrite64Bit)	(void* context, quint32 address, quint64 value);
+typedef void	(* Q32BitAddressWrite8Bit ) (void* context, quint32 address, quint8  value);
+typedef void	(* Q32BitAddressWrite16Bit) (void* context, quint32 address, quint16 value);
+typedef void	(* Q32BitAddressWrite32Bit) (void* context, quint32 address, quint32 value);
+typedef void	(* Q32BitAddressWrite64Bit) (void* context, quint32 address, quint64 value);
 
-typedef quint8	(* Q64BitAddressRead8Bit )	(void* context, quint32 address);
-typedef quint16	(* Q64BitAddressRead16Bit)	(void* context, quint32 address);
-typedef quint32 (* Q64BitAddressRead32Bit)	(void* context, quint32 address);
-typedef quint64 (* Q64BitAddressRead64Bit)	(void* context, quint32 address);
+typedef quint8	(* Q64BitAddressRead8Bit )  (void* context, quint32 address);
+typedef quint16 (* Q64BitAddressRead16Bit)  (void* context, quint32 address);
+typedef quint32 (* Q64BitAddressRead32Bit)  (void* context, quint32 address);
+typedef quint64 (* Q64BitAddressRead64Bit)  (void* context, quint32 address);
 
-typedef void	(* Q64BitAddressWrite8Bit )	(void* context, quint32 index, quint8  value);
-typedef void	(* Q64BitAddressWrite16Bit)	(void* context, quint32 index, quint16 value);
-typedef void	(* Q64BitAddressWrite32Bit)	(void* context, quint32 index, quint32 value);
-typedef void	(* Q64BitAddressWrite64Bit)	(void* context, quint32 index, quint64 value);
+typedef void	(* Q64BitAddressWrite8Bit ) (void* context, quint32 index, quint8  value);
+typedef void	(* Q64BitAddressWrite16Bit) (void* context, quint32 index, quint16 value);
+typedef void	(* Q64BitAddressWrite32Bit) (void* context, quint32 index, quint32 value);
+typedef void	(* Q64BitAddressWrite64Bit) (void* context, quint32 index, quint64 value);
 
-typedef quint8	(* QRead8Bit )			(void* context);
-typedef quint16	(* QRead16Bit)			(void* context);
-typedef quint32	(* QRead32Bit)			(void* context);
-typedef quint64	(* QRead64Bit)			(void* context);
+typedef quint8	(* QRead8Bit )		    (void* context);
+typedef quint16 (* QRead16Bit)		    (void* context);
+typedef quint32 (* QRead32Bit)		    (void* context);
+typedef quint64 (* QRead64Bit)		    (void* context);
 
-typedef void	(* QWrite8Bit )			(void* context, quint8	value);
-typedef void	(* QWrite16Bit)			(void* context, quint16 value);
-typedef void	(* QWrite32Bit)			(void* context, quint32 value);
-typedef void	(* QWrite64Bit)			(void* context, quint64 value);
+typedef void	(* QWrite8Bit )		    (void* context, quint8  value);
+typedef void	(* QWrite16Bit)		    (void* context, quint16 value);
+typedef void	(* QWrite32Bit)		    (void* context, quint32 value);
+typedef void	(* QWrite64Bit)		    (void* context, quint64 value);
 
 /* MARK: - General pourpose function types */
 
-typedef void		(* QCopy)			(void*	context,
-							 void*	output);
+typedef void	 (* QCopy)		     (void*  context,
+					      void*  output);
 	
-typedef void		(* QCopyData)			(void*	context,
-							 qsize	data_size,
-							 void*	output);
+typedef void	 (* QCopyData)		     (void*  context,
+					      qsize  data_size,
+					      void*  output);
 
-typedef qsize		(* QCopyAndSize)		(void*	context,
-							 void*	output);
+typedef qsize	 (* QCopyAndSize)	     (void*  context,
+					      void*  output);
 
-typedef qsize		(* QCopyDataAndSize)		(void*	context,
-							 qsize	data_size,
-							 void*	output);
+typedef qsize	 (* QCopyDataAndSize)	     (void*  context,
+					      qsize  data_size,
+					      void*  output);
 
-typedef void		(* QCopyRange)			(void*	context,
-							 QRange	range,
-							 void*	output);
+typedef void	 (* QCopyRange)		     (void*  context,
+					      QRange range,
+					      void*  output);
 
-typedef void		(* QCopyRangeData)		(void*	context,
-							 QRange	range,
-							 qsize	data_size,
-							 void*	output);
+typedef void	 (* QCopyRangeData)	     (void*  context,
+					      QRange range,
+					      qsize  data_size,
+					      void*  output);
 
-typedef void		(* QBoundedCopy)		(void*	context_start,
-							 void*	context_end,
-							 void*	output);
+typedef void	 (* QBoundedCopy)	     (void*  context_start,
+					      void*  context_end,
+					      void*  output);
 
-typedef void		(* QBoundedCopyData)		(void*	context_start,
-							 void*	context_end,
-							 qsize	data_size,
-							 void*	output);
+typedef void	 (* QBoundedCopyData)	     (void*  context_start,
+					      void*  context_end,
+					      qsize  data_size,
+					      void*  output);
 
-typedef qsize		(* QBoundedCopyAndSize)		(void*	context_start,
-							 void*	context_end,
-							 void*	output);
+typedef qsize	 (* QBoundedCopyAndSize)     (void*  context_start,
+					      void*  context_end,
+					      void*  output);
 
-typedef qsize		(* QBoundedCopyDataAndSize)	(void*	context_start,
-							 void*	context_end,
-							 qsize	data_size,
-							 void*	output);
+typedef qsize	 (* QBoundedCopyDataAndSize) (void*  context_start,
+					      void*  context_end,
+					      qsize  data_size,
+					      void*  output);
 
-typedef void		(* QBoundedCopyRange)		(void*	context_start,
-							 void*	context_end,
-							 void*	context_size,
-							 QRange	range,
-							 void*	output);
+typedef void	 (* QBoundedCopyRange)	     (void*  context_start,
+					      void*  context_end,
+					      void*  context_size,
+					      QRange range,
+					      void*  output);
 
-typedef void		(* QBoundedCopyRangeData)	(void*	context_start,
-							 void*	context_end,
-							 void*	context_size,
-							 QRange	range,
-							 qsize	data_size,
-							 void*	output);
+typedef void	 (* QBoundedCopyRangeData)   (void*  context_start,
+					      void*  context_end,
+					      void*  context_size,
+					      QRange range,
+					      qsize  data_size,
+					      void*  output);
 
-typedef void		(* QCopyMany)			(void*	context,
-							 quint	count,
-							 void*	output);
+typedef void	 (* QCopyMany)		     (void*  context,
+					      quint  count,
+					      void*  output);
 
-typedef void		(* QSkipOne)			(void*	input);
+typedef void	 (* QSkipOne)		     (void*  input);
 
-typedef void		(* QSkipMany)			(void*	input,
-							 quint	count);
+typedef void	 (* QSkipMany)		     (void*  input,
+					      quint  count);
 
-typedef void		(* QSwapHalves)			(void*	context);
+typedef void	 (* QSwapHalves)	     (void*  context);
 
-typedef qsize		(* QSize)			(void*	context);
+typedef qsize	 (* QSize)		     (void*  context);
 
-typedef void*		(* QElement)			(void*	context,
-							 qsize	index);
+typedef void*	 (* QElement)		     (void*  context,
+					      qsize  index);
 
-typedef void		(* QSetElement)			(void*	context,
-							 qsize	index,
-							 void*	value);
+typedef void	 (* QSetElement)	     (void*  context,
+					      qsize  index,
+					      void*  value);
 
-typedef void		(* QMoveElement)		(void*	context,
-							 qsize	old_index,
-							 qsize	new_index);
+typedef void	 (* QMoveElement)	     (void*  context,
+					      qsize  old_index,
+					      qsize  new_index);
 
-typedef qint		(* QCompareElements)		(void*	context,
-							 qsize	a_index,
-							 qsize	b_index);
+typedef qint	 (* QCompareElements)	     (void*  context,
+					      qsize  a_index,
+					      qsize  b_index);
 
-typedef void		(* QSwapElements)		(void*	context,
-							 qsize	a_index,
-							 qsize	b_index);
+typedef void	 (* QSwapElements)	     (void*  context,
+					      qsize  a_index,
+					      qsize  b_index);
 
-typedef qboolean	(* QIsValid)			(void*	context);
+typedef qboolean (* QIsValid)		     (void*  context);
 
-typedef qboolean	(* QAreEqual)			(void*	a,
-							 void*	b);
+typedef qboolean (* QAreEqual)		     (void*  a,
+					      void*  b);
 
-typedef qboolean	(* QAreEqualWithData)		(void*	a,
-							 void*	b,
-							 void*	data);
+typedef qboolean (* QAreEqualWithData)	     (void*  a,
+					      void*  b,
+					      void*  data);
 
-typedef qint		(* QCompare)			(void*	a,
-							 void*	b);
+typedef qint	 (* QCompare)		     (void*  a,
+					      void*  b);
 
-typedef qint		(* QCompareWithData)		(void*	a,
-							 void*	b,
-							 void*	data);
+typedef qint	 (* QCompareWithData)	     (void*  a,
+					      void*  b,
+					      void*  data);
 
-typedef void		(* QSwap)			(void*	a,
-							 void*	b);
+typedef void	 (* QSwap)		     (void*  a,
+					      void*  b);
 
 /* MARK: - Generic handler types */
 
@@ -275,6 +275,15 @@ typedef void (* QHandleContextIndexDoubleAndData ) (void* context, qsize index, 
 typedef void (* QHandleContextIndexLDoubleAndData) (void* context, qsize index, qldouble value,   void* data);
 typedef void (* QHandleContextIndexPointerAndData) (void* context, qsize index, void*	 pointer, void* data);
 
+#define QHandleValue(			TYPE) Q_JOIN_2(QHandle,		       Q_##TYPE##_FIXED_TYPE_Name	  )
+#define QHandleValueAndData(		TYPE) Q_JOIN_3(QHandle,		       Q_##TYPE##_FIXED_TYPE_Name, AndData)
+#define QHandleIndexAndValue(		TYPE) Q_JOIN_2(QHandleIndexAnd,	       Q_##TYPE##_FIXED_TYPE_Name	  )
+#define QHandleIndexValueAndData(	TYPE) Q_JOIN_3(QHandleIndex,	       Q_##TYPE##_FIXED_TYPE_Name, AndData)
+#define QHandleContextAndValue(		TYPE) Q_JOIN_2(QHandleContextAnd,      Q_##TYPE##_FIXED_TYPE_Name	  )
+#define QHandleContextValueAndData(	TYPE) Q_JOIN_3(QHandleContext,	       Q_##TYPE##_FIXED_TYPE_Name, AndData)
+#define QHandleContextIndexAndValue(	TYPE) Q_JOIN_2(QHandleContextIndexAnd, Q_##TYPE##_FIXED_TYPE_Name	  )
+#define QHandleContextIndexValueAndData(TYPE) Q_JOIN_3(QHandleContextIndex,    Q_##TYPE##_FIXED_TYPE_Name, AndData)
+
 /* MARK: - Generic handler with stop types */
 
 typedef void (* QHandleUInt8AndStop  )		       (quint8	 value,   qboolean *stop);
@@ -387,6 +396,15 @@ typedef void (* QHandleContextIndexDoubleDataAndStop ) (void* context, qsize ind
 typedef void (* QHandleContextIndexLDoubleDataAndStop) (void* context, qsize index, qldouble value,   void* data, qboolean *stop);
 typedef void (* QHandleContextIndexPointerDataAndStop) (void* context, qsize index, void*    pointer, void* data, qboolean *stop);
 
+#define QHandleValueAndStop(		    TYPE) Q_JOIN_3(QHandle,		Q_##TYPE##_FIXED_TYPE_Name, AndStop    )
+#define QHandleValueDataAndStop(	    TYPE) Q_JOIN_3(QHandle,		Q_##TYPE##_FIXED_TYPE_Name, DataAndStop)
+#define QHandleIndexValueAndStop(	    TYPE) Q_JOIN_3(QHandleIndex,	Q_##TYPE##_FIXED_TYPE_Name, AndStop    )
+#define QHandleIndexValueDataAndStop(	    TYPE) Q_JOIN_3(QHandleIndex,	Q_##TYPE##_FIXED_TYPE_Name, DataAndStop)
+#define QHandleContextValueAndStop(	    TYPE) Q_JOIN_3(QHandleContext,	Q_##TYPE##_FIXED_TYPE_Name, AndStop    )
+#define QHandleContextValueDataAndStop(	    TYPE) Q_JOIN_3(QHandleContext,	Q_##TYPE##_FIXED_TYPE_Name, DataAndStop)
+#define QHandleContextIndexValueAndStop(    TYPE) Q_JOIN_3(QHandleContextIndex, Q_##TYPE##_FIXED_TYPE_Name, AndStop    )
+#define QHandleContextIndexValueDataAndStop(TYPE) Q_JOIN_3(QHandleContextIndex, Q_##TYPE##_FIXED_TYPE_Name, DataAndStop)
+
 /* MARK: - Generic validator types */
 
 typedef qboolean (* QValidateUInt8  )			 (quint8   value  );
@@ -415,8 +433,9 @@ typedef qboolean (* QValidateDoubleAndData )		 (qdouble  value,   void* data);
 typedef qboolean (* QValidateLDoubleAndData)		 (qldouble value,   void* data);
 typedef qboolean (* QValidatePointerAndData)		 (void*    pointer, void* data);
 
-typedef qboolean (* QValidateIndex	    )		 (qsize index		       );
-typedef qboolean (* QValidateIndexAndData   )		 (qsize index, void*	data   );
+typedef qboolean (* QValidateIndex	    )		 (qsize index		 );
+typedef qboolean (* QValidateIndexAndData   )		 (qsize index, void* data);
+
 typedef qboolean (* QValidateIndexAndUInt8  )		 (qsize index, quint8	value  );
 typedef qboolean (* QValidateIndexAndUInt16 )		 (qsize index, quint16	value  );
 typedef qboolean (* QValidateIndexAndUInt32 )		 (qsize index, quint32	value  );
@@ -497,5 +516,14 @@ typedef qboolean (* QValidateContextIndexFloatAndData  ) (void* context, qsize i
 typedef qboolean (* QValidateContextIndexDoubleAndData ) (void* context, qsize index, qdouble  value,	void* data);
 typedef qboolean (* QValidateContextIndexLDoubleAndData) (void* context, qsize index, qldouble value,	void* data);
 typedef qboolean (* QValidateContextIndexPointerAndData) (void* context, qsize index, void*    pointer, void* data);
+
+#define QValidateValue(			  TYPE) Q_JOIN_2(QValidate,		   Q_##TYPE##_FIXED_TYPE_Name	      )
+#define QValidateValueAndData(		  TYPE) Q_JOIN_3(QValidate,		   Q_##TYPE##_FIXED_TYPE_Name, AndData)
+#define QValidateIndexAndValue(		  TYPE) Q_JOIN_2(QValidateIndexAnd,	   Q_##TYPE##_FIXED_TYPE_Name	      )
+#define QValidateIndexValueAndData(	  TYPE) Q_JOIN_3(QValidateIndex,	   Q_##TYPE##_FIXED_TYPE_Name, AndData)
+#define QValidateContextAndValue(	  TYPE) Q_JOIN_2(QValidateContextAnd,      Q_##TYPE##_FIXED_TYPE_Name	      )
+#define QValidateContextValueAndData(	  TYPE) Q_JOIN_3(QValidateContext,	   Q_##TYPE##_FIXED_TYPE_Name, AndData)
+#define QValidateContextIndexAndValue(	  TYPE) Q_JOIN_2(QValidateContextIndexAnd, Q_##TYPE##_FIXED_TYPE_Name	      )
+#define QValidateContextIndexValueAndData(TYPE) Q_JOIN_3(QValidateContextIndex,    Q_##TYPE##_FIXED_TYPE_Name, AndData)
 
 #endif /* __Q_types_generic_functions_H__ */
