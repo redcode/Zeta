271220e656aa5a27a1a66585d455f02a20b3cb2e
redcode <manuel@redcodesoftware.com>
2014-03-14 18:17:45 +0100

    functions/time.h updated

--- Q.API/1.0/C/Q/functions/date.h
+++ Q.API/1.0/C/Q/functions/date.h
@@ -1,4 +1,4 @@
-/* Q API - functions/time.h
+/* Q API - functions/date.h
 	      __	   __
   _______ ___/ /______ ___/ /__
  / __/ -_) _  / __/ _ \ _  / -_)
@@ -6,11 +6,11 @@
 Copyright © 2006-2014 Manuel Sainz de Baranda y Goñi.
 Released under the terms of the GNU General Public License v3. */
 
-#ifndef __Q_functions_time_H__
-#define __Q_functions_time_H__
+#ifndef __Q_functions_date_H__
+#define __Q_functions_date_H__
 
 #include <Q/types/base.h>
-#include <Q/macros/time.h>
+#include <Q/macros/date.h>
 
 
 Q_INLINE qboolean q_gregorian_year_is_leap(quint64 year)
@@ -25,4 +25,4 @@ Q_INLINE quint8 q_gregorian_year_days_in_month(quint64 year, quint8 month)
 	}
 
 
-#endif /* __Q_functions_time_H__ */
+#endif /* __Q_functions_date_H__ */
