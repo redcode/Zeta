3656499eaed2ec43f36011fcc7a3176a63f797d3
redcode <manuel@redcodesoftware.com>
2014-10-26 13:35:10 +0100

    WIP

--- Q.API/v1.0/C/Q/types/data.h
+++ Q.API/v1.0/C/Q/types/data.h
@@ -12,6 +12,8 @@ Released under the terms of the GNU General Public License v3. */
 #include <Q/types/base.h>
 #include <Q/macros/node.h>
 
+/* MARK: - Arrays */
+
 typedef struct {
 	void* data;
 	qsize size;
@@ -22,6 +24,8 @@ typedef struct {
 	qsize size;
 	qsize block_size;
 } QBlockArray;
+
+/* MARK: - Nodes */
 
 Q_DEFINE_LIST_NODE	 (QListNode,	  );
 Q_DEFINE_CHAIN_NODE	 (QChainNode,	  );
@@ -38,19 +42,21 @@ Q_DEFINE_CHAIN_NODE	 (QKeyDataChainNode,	  void *data; quintptr key;);
 Q_DEFINE_TREE_NODE	 (QKeyDataTreeNode,	  void *data; quintptr key;);
 Q_DEFINE_BINARY_TREE_NODE(QKeyDataBinaryTreeNode, void *data; quintptr key;);
 
-#define Q_ARRAY(		     p) ((QArray		 *)(p))
-#define Q_BLOCK_ARRAY(		     p) ((QBlockArray		 *)(p))
-#define Q_LIST_NODE(		     p) ((QListNode		 *)(p))
-#define Q_CHAIN_NODE(		     p) ((QChainNode		 *)(p))
-#define Q_TREE_NODE(		     p) ((QTreeNode		 *)(p))
-#define Q_BINARY_TREE_NODE	     p) ((QBinaryTreeNode	 *)(p))
-#define Q_DATA_LIST_NODE(	     p) ((QDataListNode		 *)(p))
-#define Q_DATA_CHAIN_NODE(	     p) ((QDataChainNode	 *)(p))
-#define Q_DATA_TREE_NODE(	     p) ((QDataTreeNode		 *)(p))
-#define Q_DATA_BINARY_TREE_NODE(     p) ((QDataBinaryTreeNode	 *)(p))
-#define Q_KEY_DATA_LIST_NODE(	     p) ((QKeyDataListNode	 *)(p))
-#define Q_KEY_DATA_CHAIN_NODE(	     p) ((QKeyDataChainNode	 *)(p))
-#define Q_KEY_DATA_TREE_NODE(	     p) ((QKeyDataTreeNode	 *)(p))
-#define Q_KEY_DATA_BINARY_TREE_NODE( p) ((QKeyDataBinaryTreeNode *)(p))
+/* MARK: - Casts */
+
+#define Q_ARRAY(		    p) ((QArray			*)(p))
+#define Q_BLOCK_ARRAY(		    p) ((QBlockArray		*)(p))
+#define Q_LIST_NODE(		    p) ((QListNode		*)(p))
+#define Q_CHAIN_NODE(		    p) ((QChainNode		*)(p))
+#define Q_TREE_NODE(		    p) ((QTreeNode		*)(p))
+#define Q_BINARY_TREE_NODE	    p) ((QBinaryTreeNode	*)(p))
+#define Q_DATA_LIST_NODE(	    p) ((QDataListNode		*)(p))
+#define Q_DATA_CHAIN_NODE(	    p) ((QDataChainNode		*)(p))
+#define Q_DATA_TREE_NODE(	    p) ((QDataTreeNode		*)(p))
+#define Q_DATA_BINARY_TREE_NODE(    p) ((QDataBinaryTreeNode	*)(p))
+#define Q_KEY_DATA_LIST_NODE(	    p) ((QKeyDataListNode	*)(p))
+#define Q_KEY_DATA_CHAIN_NODE(	    p) ((QKeyDataChainNode	*)(p))
+#define Q_KEY_DATA_TREE_NODE(	    p) ((QKeyDataTreeNode	*)(p))
+#define Q_KEY_DATA_BINARY_TREE_NODE(p) ((QKeyDataBinaryTreeNode *)(p))
 
 #endif /* __Q_types_data_H__ */
