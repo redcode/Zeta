49a8a266545ee21fa8245e001f71b97019e3a8a6
redcode <manuel@redcodesoftware.com>
2014-12-01 00:20:32 +0100

    homogeneity

--- Q.API/v1.0/C/Q/types/data.h
+++ Q.API/v1.0/C/Q/types/data.h
@@ -29,13 +29,13 @@ typedef struct {
 
 Q_DEFINE_LIST_NODE	 (QListNode,	  );
 Q_DEFINE_CHAIN_NODE	 (QChainNode,	  );
-Q_DEFINE_TREE_NODE	 (QTreeNode,	  );
 Q_DEFINE_BINARY_TREE_NODE(QBinaryTreeNode,);
+Q_DEFINE_TREE_NODE	 (QTreeNode,	  );
 
 Q_DEFINE_LIST_NODE	 (QDataListNode,       void* data;);
 Q_DEFINE_CHAIN_NODE	 (QDataChainNode,      void* data;);
-Q_DEFINE_TREE_NODE	 (QDataTreeNode,       void* data;);
 Q_DEFINE_BINARY_TREE_NODE(QDataBinaryTreeNode, void* data;);
+Q_DEFINE_TREE_NODE	 (QDataTreeNode,       void* data;);
 
 typedef struct {
 	QListNode* first;
@@ -50,19 +50,19 @@ typedef struct {
 } QChain;
 
 typedef struct {
+	QBinaryTreeNode* root;
+	qsize		 size;
+} QBinaryTree;
+
+typedef struct {
 	QTreeNode* root;
 	qsize	   size;
 } QTree;
 
 typedef struct {
-	QTree*	  tree;
+	QTree*	   tree;
 	QTreeNode* node;
 } QTreeCursor;
-
-typedef struct {
-	QBinaryTreeNode* root;
-	qsize		 size;
-} QBinaryTree;
 
 /* MARK: - Casts */
 
@@ -70,16 +70,16 @@ typedef struct {
 #define Q_BLOCK_ARRAY(		p) ((QBlockArray	 *)(p))
 #define Q_LIST_NODE(		p) ((QListNode		 *)(p))
 #define Q_CHAIN_NODE(		p) ((QChainNode		 *)(p))
-#define Q_TREE_NODE(		p) ((QTreeNode		 *)(p))
 #define Q_BINARY_TREE_NODE(	p) ((QBinaryTreeNode	 *)(p))
+#define Q_TREE_NODE(		p) ((QTreeNode		 *)(p))
 #define Q_DATA_LIST_NODE(	p) ((QDataListNode	 *)(p))
 #define Q_DATA_CHAIN_NODE(	p) ((QDataChainNode	 *)(p))
-#define Q_DATA_TREE_NODE(	p) ((QDataTreeNode	 *)(p))
 #define Q_DATA_BINARY_TREE_NODE(p) ((QDataBinaryTreeNode *)(p))
+#define Q_DATA_TREE_NODE(	p) ((QDataTreeNode	 *)(p))
 #define Q_LIST(			p) ((QList		 *)(p))
 #define Q_CHAIN(		p) ((QChain		 *)(p))
+#define Q_BINARY_TREE(		p) ((QBinaryTree	 *)(p))
 #define Q_TREE(			p) ((QTree		 *)(p))
 #define Q_TREE_CURSOR(		p) ((QTreeCursor	 *)(p))
-#define Q_BINARY_TREE(		p) ((QBinaryTree	 *)(p))
 
 #endif /* __Q_types_data_H__ */
