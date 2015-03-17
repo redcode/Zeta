e8e058979929bcc8f08717716ea67713bd372930
redcode <manuel@redcodesoftware.com>
2015-03-17 21:43:53 +0100

    <Q/functions/base/all.h>

---
+++ Q.Kit/v0.1/API/C/Q/functions/base/all.h
@@ -0,0 +1,17 @@
+/* Q Kit C API - functions/base/all.h
+	      __	   __
+  _______ ___/ /______ ___/ /__
+ / __/ -_) _  / __/ _ \ _  / -_)
+/_/  \__/\_,_/\__/\___/_,_/\__/
+Copyright © 2006-2015 Manuel Sainz de Baranda y Goñi.
+Released under the terms of the GNU Lesser General Public License v3. */
+
+#ifndef __Q_functions_base_all_H__
+#define __Q_functions_base_all_H__
+
+#include <Q/functions/base/block.h>
+#include <Q/functions/base/Q2D.h>
+#include <Q/functions/base/Q3D.h>
+#include <Q/functions/base/QRange.h>
+
+#endif /* __Q_functions_base_all_H__ */
