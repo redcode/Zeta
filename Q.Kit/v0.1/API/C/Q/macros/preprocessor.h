5624b141718720c96fcc7d20f74f49fcf9c1dbcf
redcode <manuel@redcodesoftware.com>
2015-03-26 22:03:21 +0100

    Original copyright notice of some files.

--- Q.Kit/v0.1/API/C/Q/macros/preprocessor.h
+++ Q.Kit/v0.1/API/C/Q/macros/preprocessor.h
@@ -3,6 +3,7 @@
   _______ ___/ /______ ___/ /__
  / __/ -_) _  / __/ _ \ _  / -_)
 /_/  \__/\_,_/\__/\___/_,_/\__/
+Copyright © 1989 КГБ СССР.
 Copyright © 2006-2015 Manuel Sainz de Baranda y Goñi.
 Copyright © 2006 Laurent Deniau <laurent.deniau@cern.ch>.
 Released under the terms of the GNU Lesser General Public License v3. */
