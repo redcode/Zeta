b5d0c92e7703a110dd75b535d04811a218dedbbf
redcode <manuel@redcodesoftware.com>
2015-04-19 05:55:46 +0200

    conventions

--- Q.Kit/v0.1/API/C/Q/functions/geometry/Q3DVector.h
+++ Q.Kit/v0.1/API/C/Q/functions/geometry/Q3DVector.h
@@ -16,14 +16,11 @@ Released under the terms of the GNU Lesser General Public License v3. */
 #define Q_IMPLEMENTATION_3D_VECTOR(Type, type)							\
 												\
 												\
-Q_INLINE qboolean q_3d_##type##_vector_are_equal(Q3D##Type##Vector a, Q3D##Type##Vector b)	\
+Q_INLINE qboolean q_3d_vector_##type##_are_equal(Q3DVector##Type a, Q3DVector##Type b)		\
 	{return q_3d_##type##_are_equal(a.a, b.a) && q_3d_##type##_are_equal(a.b, b.b);}	\
 												\
 												\
-Q_INLINE qboolean q_3d_##type##_vector_are_perpendicular(					\
-	Q3D##Type##Vector a,									\
-	Q3D##Type##Vector b									\
-)												\
+Q_INLINE qboolean q_3d_vector_##type##_are_perpendicular(Q3DVector##Type a, Q3DVector##Type b)	\
 	{											\
 	return q_3d_##type##_are_perpendicular							\
 		(q_3d_##type##_subtract(a.b, a.a),						\
@@ -31,15 +28,14 @@ Q_INLINE qboolean q_3d_##type##_vector_are_perpendicular(					\
 	}											\
 												\
 												\
-Q_INLINE qboolean q_3d_##type##_vector_is_zero(Q3D##Type##Vector object)			\
+Q_INLINE qboolean q_3d_vector_##type##_is_zero(Q3DVector##Type object)				\
 	{return q_3d_##type##_is_zero(object.a) && q_3d_##type##_is_zero(object.b);}		\
 												\
 												\
-Q_INLINE Q3D##Type##Vector q_3d_##type##_vector_reversed(Q3D##Type##Vector object)		\
+Q_INLINE Q3DVector##Type q_3d_vector_##type##_reversed(Q3DVector##Type object)			\
 	{											\
-	return q_3d_##type##_vector								\
-		(object.b.x, object.b.y, object.b.z,						\
-		 object.a.x, object.a.y, object.a.z);						\
+	return q_3d_vector_##type								\
+		(object.b.x, object.b.y, object.b.z, object.a.x, object.a.y, object.a.z);	\
 	}											\
 
 
@@ -50,9 +46,9 @@ Q_IMPLEMENTATION_3D_VECTOR(LDouble, ldouble)
 
 /* MARK: - Default real type definitions */
 
-#define q_3d_vector_are_equal	      Q_JOIN_3(q_3d_, Q_REAL_FIXED_TYPE_name, _vector_are_equal	       )
-#define q_3d_vector_are_perpendicular Q_JOIN_3(q_3d_, Q_REAL_FIXED_TYPE_name, _vector_are_perpendicular)
-#define q_3d_vector_is_zero	      Q_JOIN_3(q_3d_, Q_REAL_FIXED_TYPE_name, _vector_is_zero	       )
-#define q_3d_vector_reversed	      Q_JOIN_3(q_3d_, Q_REAL_FIXED_TYPE_name, _vector_reversed	       )
+#define q_3d_vector_are_equal	      Q_JOIN_3(q_3d_vector_, Q_REAL_FIXED_TYPE_name, _are_equal	       )
+#define q_3d_vector_are_perpendicular Q_JOIN_3(q_3d_vector_, Q_REAL_FIXED_TYPE_name, _are_perpendicular)
+#define q_3d_vector_is_zero	      Q_JOIN_3(q_3d_vector_, Q_REAL_FIXED_TYPE_name, _is_zero	       )
+#define q_3d_vector_reversed	      Q_JOIN_3(q_3d_vector_, Q_REAL_FIXED_TYPE_name, _reversed	       )
 
 #endif /* __Q_functions_geometry_Q3DVector_H__ */
