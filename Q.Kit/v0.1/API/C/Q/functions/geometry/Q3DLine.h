6238f106d7a894340f1cd936abf3e56abfeba269
redcode <manuel@redcodesoftware.com>
2015-04-19 05:47:22 +0200

    conventions

--- Q.Kit/v0.1/API/C/Q/functions/geometry/Q3DLine.h
+++ Q.Kit/v0.1/API/C/Q/functions/geometry/Q3DLine.h
@@ -11,30 +11,30 @@ Released under the terms of the GNU Lesser General Public License v3. */
 
 #include <Q/functions/geometry/Q3DVector.h>
 
-#define q_3d_float_line_are_equal	    q_3d_float_vector_are_equal
-#define q_3d_float_line_are_perpendicular   q_3d_float_vector_are_perpendicular
-#define q_3d_float_line_is_zero		    q_3d_float_vector_is_zero
-#define q_3d_float_line_reversed	    q_3d_float_vector_reversed
+#define q_3d_line_float_are_equal	    q_3d_vector_float_are_equal
+#define q_3d_line_float_are_perpendicular   q_3d_vector_float_are_perpendicular
+#define q_3d_line_float_is_zero		    q_3d_vector_float_is_zero
+#define q_3d_line_float_reversed	    q_3d_vector_float_reversed
 
-#define q_3d_double_line_are_equal	    q_3d_double_vector_are_equal
-#define q_3d_double_line_are_perpendicular  q_3d_double_vector_are_perpendicular
-#define q_3d_double_line_is_zero	    q_3d_double_vector_is_zero
-#define q_3d_double_line_reversed	    q_3d_double_vector_reversed
+#define q_3d_line_double_are_equal	    q_3d_vector_double_are_equal
+#define q_3d_line_double_are_perpendicular  q_3d_vector_double_are_perpendicular
+#define q_3d_line_double_is_zero	    q_3d_vector_double_is_zero
+#define q_3d_line_double_reversed	    q_3d_vector_double_reversed
 
-#define q_3d_ldouble_line_are_equal	    q_3d_ldouble_vector_are_equal
-#define q_3d_ldouble_line_are_perpendicular q_3d_ldouble_vector_are_perpendicular
-#define q_3d_ldouble_line_is_zero	    q_3d_ldouble_vector_is_zero
-#define q_3d_ldouble_line_reversed	    q_3d_ldouble_vector_reversed
+#define q_3d_line_ldouble_are_equal	    q_3d_vector_ldouble_are_equal
+#define q_3d_line_ldouble_are_perpendicular q_3d_vector_ldouble_are_perpendicular
+#define q_3d_line_ldouble_is_zero	    q_3d_vector_ldouble_is_zero
+#define q_3d_line_ldouble_reversed	    q_3d_vector_ldouble_reversed
 
 
-#define Q_IMPLEMENTATION_3D_LINE_SEGMENT(Type, type, _)						\
-												\
-												\
-Q_INLINE Q3D##Type q_3d_##type##_line_segment_center(Q3D##Type##Line object)			\
-	{return q_3d_##type##_middle(object.a, object.b);}					\
-												\
-												\
-Q_INLINE Q3D##Type q_3d_##type##_line_segment_lerp(Q3D##Type##Line object, q##type alpha)	\
+#define Q_IMPLEMENTATION_3D_LINE_SEGMENT(Type, type, _)					\
+											\
+											\
+Q_INLINE Q3D##Type q_3d_line_segment_##type##_center(Q3DLine##Type object)		\
+	{return q_3d_##type##_middle(object.a, object.b);}				\
+											\
+											\
+Q_INLINE Q3D##Type q_3d_line_segment_##type##_lerp(Q3DLine##Type object, q##type alpha)	\
 	{return q_3d_##type##_lerp(object.a, object.b, alpha);}
 
 
@@ -50,7 +50,7 @@ Q_IMPLEMENTATION_3D_LINE_SEGMENT(LDouble, ldouble, Q_LDOUBLE)
 #define q_3d_line_is_zero	    q_3d_vector_is_zero
 #define q_3d_line_reversed	    q_3d_vector_reversed
 
-#define q_3d_line_segment_center    Q_JOIN_3(q_3d_, Q_REAL_FIXED_TYPE_name, _line_segment_center)
-#define q_3d_line_segment_lerp	    Q_JOIN_3(q_3d_, Q_REAL_FIXED_TYPE_name, _line_segment_lerp  )
+#define q_3d_line_segment_center    Q_JOIN_3(q_3d_line_segment_, Q_REAL_FIXED_TYPE_name, _center)
+#define q_3d_line_segment_lerp	    Q_JOIN_3(q_3d_line_segment_, Q_REAL_FIXED_TYPE_name, _lerp	)
 
 #endif /* __Q_functions_geometry_Q3DLine_H__ */
