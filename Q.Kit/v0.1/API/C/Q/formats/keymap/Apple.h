f9f9b4880414036117ad5434879e3cb2548cb980
redcode <manuel@redcodesoftware.com>
2015-06-26 09:24:06 +0200

    Apple keymaps.

---
+++ Q.Kit/v0.1/API/C/Q/formats/keymap/Apple.h
@@ -0,0 +1,60 @@
+/* Q Kit C API - formats/keymap/Apple.h
+	      __	   __
+  _______ ___/ /______ ___/ /__
+ / __/ -_) _  / __/ _ \ _  / -_)
+/_/  \__/\_,_/\__/\___/_,_/\__/
+Copyright © 2006-2015 Manuel Sainz de Baranda y Goñi.
+Released under the terms of the GNU Lesser General Public License v3. */
+
+#ifndef __Q_formats_keymap_Apple_H__
+#define __Q_formats_keymap_Apple_H__
+
+#define Q_KEYMAP_APPLE_RETURN		0x24
+#define Q_KEYMAP_APPLE_TAB		0x30
+#define Q_KEYMAP_APPLE_SPACE		0x31
+#define Q_KEYMAP_APPLE_DELETE		0x33
+#define Q_KEYMAP_APPLE_ESCAPE		0x35
+#define Q_KEYMAP_APPLE_COMMAND		0x37
+#define Q_KEYMAP_APPLE_SHIFT		0x38
+#define Q_KEYMAP_APPLE_CAPSLOCK		0x39
+#define Q_KEYMAP_APPLE_OPTION		0x3A
+#define Q_KEYMAP_APPLE_CONTROL		0x3B
+#define Q_KEYMAP_APPLE_RIGHT_SHIFT	0x3C
+#define Q_KEYMAP_APPLE_RIGHT_OPTION	0x3D
+#define Q_KEYMAP_APPLE_RIGHT_CONTROL	0x3E
+#define Q_KEYMAP_APPLE_FUNCTION		0x3F
+#define Q_KEYMAP_APPLE_F17		0x40
+#define Q_KEYMAP_APPLE_VOLUME_UP	0x48
+#define Q_KEYMAP_APPLE_VOLUME_DOWN	0x49
+#define Q_KEYMAP_APPLE_MUTE		0x4A
+#define Q_KEYMAP_APPLE_F18		0x4F
+#define Q_KEYMAP_APPLE_F19		0x50
+#define Q_KEYMAP_APPLE_F20		0x5A
+#define Q_KEYMAP_APPLE_F5		0x60
+#define Q_KEYMAP_APPLE_F6		0x61
+#define Q_KEYMAP_APPLE_F7		0x62
+#define Q_KEYMAP_APPLE_F3		0x63
+#define Q_KEYMAP_APPLE_F8		0x64
+#define Q_KEYMAP_APPLE_F9		0x65
+#define Q_KEYMAP_APPLE_F11		0x67
+#define Q_KEYMAP_APPLE_F13		0x69
+#define Q_KEYMAP_APPLE_F16		0x6A
+#define Q_KEYMAP_APPLE_F14		0x6B
+#define Q_KEYMAP_APPLE_F10		0x6D
+#define Q_KEYMAP_APPLE_F12		0x6F
+#define Q_KEYMAP_APPLE_F15		0x71
+#define Q_KEYMAP_APPLE_HELP		0x72
+#define Q_KEYMAP_APPLE_HOME		0x73
+#define Q_KEYMAP_APPLE_PAGE_UP		0x74
+#define Q_KEYMAP_APPLE_FORWARD_DELETE	0x75
+#define Q_KEYMAP_APPLE_F4		0x76
+#define Q_KEYMAP_APPLE_END		0x77
+#define Q_KEYMAP_APPLE_F2		0x78
+#define Q_KEYMAP_APPLE_PAGE_DOWN	0x79
+#define Q_KEYMAP_APPLE_F1		0x7A
+#define Q_KEYMAP_APPLE_LEFT_ARROW	0x7B
+#define Q_KEYMAP_APPLE_RIGHT_ARROW	0x7C
+#define Q_KEYMAP_APPLE_DOWN_ARROW	0x7D
+#define Q_KEYMAP_APPLE_UP_ARROW		0x7E
+
+#endif /* __Q_formats_keymap_Apple_H__ */
