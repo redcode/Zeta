46d276b3b1b47a746991597f317a56d1c130008c
redcode <manuel@redcodesoftware.com>
2015-06-27 21:03:42 +0200

    Character sets.

--- Q.Kit/v0.1/API/C/Q/formats/character set/DOS CP869.h
+++ Q.Kit/v0.1/API/C/Q/formats/character set/DOS CP869.h
@@ -10,14 +10,14 @@ Released under the terms of the GNU Lesser General Public License v3. */
 #define __Q_formats_character_set_DOS_CP869_H__
 
 /* From 00h to 7Fh like ASCII */
-/* 80h is undefined */
-/* 81h is undefined */
-/* 82h is undefined */
-/* 83h is undefined */
-/* 84h is undefined */
-/* 85h is undefined */
+/* 80h undefined */
+/* 81h undefined */
+/* 82h undefined */
+/* 83h undefined */
+/* 84h undefined */
+/* 85h undefined */
 #define Q_DOS_CP869_GREEK_CAPITAL_LETTER_ALPHA_WITH_TONOS		0x86
-/* 87h is undefined */
+/* 87h undefined */
 #define Q_DOS_CP869_MIDDLE_DOT						0x88
 #define Q_DOS_CP869_NOT_SIGN						0x89
 #define Q_DOS_CP869_BROKEN_BAR						0x8A
@@ -29,8 +29,8 @@ Released under the terms of the GNU Lesser General Public License v3. */
 #define Q_DOS_CP869_GREEK_CAPITAL_LETTER_IOTA_WITH_TONOS		0x90
 #define Q_DOS_CP869_GREEK_CAPITAL_LETTER_IOTA_WITH_DIALYTIKA		0x91
 #define Q_DOS_CP869_GREEK_CAPITAL_LETTER_OMICRON_WITH_TONOS		0x92
-/* 93h is undefined */
-/* 94h is undefined */
+/* 93h undefined */
+/* 94h undefined */
 #define Q_DOS_CP869_GREEK_CAPITAL_LETTER_UPSILON_WITH_TONOS		0x95
 #define Q_DOS_CP869_GREEK_CAPITAL_LETTER_UPSILON_WITH_DIALYTIKA		0x96
 #define Q_DOS_CP869_COPYRIGHT_SIGN					0x97
@@ -139,22 +139,22 @@ Released under the terms of the GNU Lesser General Public License v3. */
 #define Q_DOS_CP869_BLACK_SQUARE					0xFE
 #define Q_DOS_CP869_NO_BREAK_SPACE					0xFF
 
-#define Q_ARRAY_CONTENT_DOS_CP869_TO_UNICODE(_)					\
-	_(0000), _(0000), _(0000), _(0000), _(0000), _(0000), _(0386), _(0000), \
-	_(00B7), _(00AC), _(00A6), _(2018), _(2019), _(0388), _(2015), _(0389), \
-	_(038A), _(03AA), _(038C), _(0000), _(0000), _(038E), _(03AB), _(00A9), \
-	_(038F), _(00B2), _(00B3), _(03AC), _(00A3), _(03AD), _(03AE), _(03AF), \
-	_(03CA), _(0390), _(03CC), _(03CD), _(0391), _(0392), _(0393), _(0394), \
-	_(0395), _(0396), _(0397), _(00BD), _(0398), _(0399), _(00AB), _(00BB), \
-	_(2591), _(2592), _(2593), _(2502), _(2524), _(039A), _(039B), _(039C), \
-	_(039D), _(2563), _(2551), _(2557), _(255D), _(039E), _(039F), _(2510), \
-	_(2514), _(2534), _(252C), _(251C), _(2500), _(253C), _(03A0), _(03A1), \
-	_(255A), _(2554), _(2569), _(2566), _(2560), _(2550), _(256C), _(03A3), \
-	_(03A4), _(03A5), _(03A6), _(03A7), _(03A8), _(03A9), _(03B1), _(03B2), \
-	_(03B3), _(2518), _(250C), _(2588), _(2584), _(03B4), _(03B5), _(2580), \
-	_(03B6), _(03B7), _(03B8), _(03B9), _(03BA), _(03BB), _(03BC), _(03BD), \
-	_(03BE), _(03BF), _(03C0), _(03C1), _(03C3), _(03C2), _(03C4), _(0384), \
-	_(00AD), _(00B1), _(03C5), _(03C6), _(03C7), _(00A7), _(03C8), _(0385), \
-	_(00B0), _(00A8), _(03C9), _(03CB), _(03B0), _(03CE), _(25A0), _(00A0)
+#define Q_ARRAY_CONTENT_DOS_CP869_TO_UNICODE(_, undefined)				      \
+	undefined, undefined, undefined, undefined, undefined, undefined, _(0386), undefined, \
+	_(00B7),   _(00AC),   _(00A6),	 _(2018),   _(2019),   _(0388),	  _(2015), _(0389),   \
+	_(038A),   _(03AA),   _(038C),	 undefined, undefined, _(038E),	  _(03AB), _(00A9),   \
+	_(038F),   _(00B2),   _(00B3),	 _(03AC),   _(00A3),   _(03AD),	  _(03AE), _(03AF),   \
+	_(03CA),   _(0390),   _(03CC),	 _(03CD),   _(0391),   _(0392),	  _(0393), _(0394),   \
+	_(0395),   _(0396),   _(0397),	 _(00BD),   _(0398),   _(0399),	  _(00AB), _(00BB),   \
+	_(2591),   _(2592),   _(2593),	 _(2502),   _(2524),   _(039A),	  _(039B), _(039C),   \
+	_(039D),   _(2563),   _(2551),	 _(2557),   _(255D),   _(039E),	  _(039F), _(2510),   \
+	_(2514),   _(2534),   _(252C),	 _(251C),   _(2500),   _(253C),	  _(03A0), _(03A1),   \
+	_(255A),   _(2554),   _(2569),	 _(2566),   _(2560),   _(2550),	  _(256C), _(03A3),   \
+	_(03A4),   _(03A5),   _(03A6),	 _(03A7),   _(03A8),   _(03A9),	  _(03B1), _(03B2),   \
+	_(03B3),   _(2518),   _(250C),	 _(2588),   _(2584),   _(03B4),	  _(03B5), _(2580),   \
+	_(03B6),   _(03B7),   _(03B8),	 _(03B9),   _(03BA),   _(03BB),	  _(03BC), _(03BD),   \
+	_(03BE),   _(03BF),   _(03C0),	 _(03C1),   _(03C3),   _(03C2),	  _(03C4), _(0384),   \
+	_(00AD),   _(00B1),   _(03C5),	 _(03C6),   _(03C7),   _(00A7),	  _(03C8), _(0385),   \
+	_(00B0),   _(00A8),   _(03C9),	 _(03CB),   _(03B0),   _(03CE),	  _(25A0), _(00A0)
 
 #endif /* __Q_formats_character_set_DOS_CP869_H__ */
