ec04fbf067db751f5bcdcb9615a5310e764ef701
redcode <manuel@redcodesoftware.com>
2015-06-30 06:46:03 +0200

    Q_CAST macro arguments' order.

--- Q.Kit/v0.1/API/C/Q/macros/casting.h
+++ Q.Kit/v0.1/API/C/Q/macros/casting.h
@@ -13,7 +13,7 @@ Released under the terms of the GNU Lesser General Public License v3. */
 
 #if Q_LANGUAGE_HAS(COMPOUND_LITERAL) && Q_LANGUAGE_HAS(ANONYMOUS_UNION)
 
-#	define Q_CAST(structure, from_type, to_type) \
+#	define Q_CAST(from_type, to_type, structure) \
 		((union {from_type from; to_type to;}){.from = structure}).to
 
 #	define Q_AVAILABLE_CAST_MACRO
