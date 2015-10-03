9d78b9181ae0f7a1ba6bc4070d6dfa28f3b422aa
redcode <manuel@redcodesoftware.com>
2015-10-03 23:06:47 +0200

    Formats

---
+++ Z.Kit/v0.1/API/C/Z/formats/cipher/Blowfish.h
@@ -0,0 +1,17 @@
+/* Z Kit C API - formats/cipher/Blowfish.h
+	      __	   __
+  _______ ___/ /______ ___/ /__
+ / __/ -_) _  / __/ _ \ _  / -_)
+/_/  \__/\_,_/\__/\___/_,_/\__/
+Copyright © 2006-2015 Manuel Sainz de Baranda y Goñi.
+Released under the terms of the GNU Lesser General Public License v3. */
+
+#ifndef __Z_formats_cipher_Blowfish_H__
+#define __Z_formats_cipher_Blowfish_H__
+
+#define Z_BLOWFISH_KEY_MINIMUM_SIZE 8
+#define Z_BLOWFISH_KEY_MAXIMUM_SIZE 56
+#define Z_BLOWFISH_KEY_WORD_SIZE    1
+#define Z_BLOWFISH_WORD_SIZE	    8
+
+#endif /* __Z_formats_cipher_Blowfish_H__ */
