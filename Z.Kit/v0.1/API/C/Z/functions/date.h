ed93f5668c80d922dc4a813928ea80eca00156e9
redcode <manuel@redcodesoftware.com>
2015-10-21 11:34:06 +0200

    Changed year arguments from zuint64 to zullong in date functions.

--- Z.Kit/v0.1/API/C/Z/functions/date.h
+++ Z.Kit/v0.1/API/C/Z/functions/date.h
@@ -13,11 +13,11 @@ Released under the terms of the GNU Lesser General Public License v3. */
 #include <Z/macros/date.h>
 
 
-Z_INLINE zboolean z_gregorian_year_is_leap(zuint64 year)
+Z_INLINE zboolean z_gregorian_year_is_leap(zullong year)
 	{return Z_GREGORIAN_YEAR_IS_LEAP(year);}
 
 
-Z_INLINE zuint8 z_gregorian_year_days_in_month(zuint64 year, zuint8 month)
+Z_INLINE zuint8 z_gregorian_year_days_in_month(zullong year, zuint8 month)
 	{
 	if (month == 2) return z_gregorian_year_is_leap(year) ? 29 : 28;
 	if (month == 4 || month == 6 || month == 9 || month == 11) return 30;
