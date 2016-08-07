009307fb771f1647035aabc2d7469bf636142d84
redcode <manuel@redcodesoftware.com>
2016-08-07 14:59:11 +0200

    Shared pointers.

--- Z.Kit/v0.1/API/C++/Z/classes/memory/Shared.hpp
+++ Z.Kit/v0.1/API/C++/Z/classes/memory/Shared.hpp
@@ -12,7 +12,6 @@ Released under the terms of the GNU Lesser General Public License v3. */
 
 #include <Z/types/base.hpp>
 #include <Z/macros/pointer.h>
-#include <stdio.h>
 
 #if Z_LANGUAGE_HAS(CPP, VARIADIC_TEMPLATE)
 
@@ -28,8 +27,6 @@ Released under the terms of the GNU Lesser General Public License v3. */
 
 				template <class ...A>
 				inline Container(A... arguments) : owner_count(0), object(arguments...) {}
-				inline ~Container()
-					{printf("~Container()\n");}
 			};
 
 			Container *container;
@@ -74,7 +71,7 @@ Released under the terms of the GNU Lesser General Public License v3. */
 		};
 
 
-		template <typename T, class ...A> Z_INLINE T *new_shared(A...arguments)
+		template <typename T, class ...A> Z_INLINE T *shared_new(A...arguments)
 			{
 			typename Shared<T>::Container *container =
 			new typename Shared<T>::Container(arguments...);
@@ -84,7 +81,7 @@ Released under the terms of the GNU Lesser General Public License v3. */
 			}
 
 
-		template <typename T> Z_INLINE void delete_shared(T *object)
+		template <typename T> Z_INLINE void shared_delete(T *object)
 			{
 			delete Z_BOP
 				(typename Shared<T>::Container *, object,
