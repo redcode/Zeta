737c24896fbcb1481530c441c4c2a3105f2c4ec0
redcode <manuel@redcodesoftware.com>
2016-10-10 06:36:23 +0200

    Traits.

--- Z.Kit/v0.1/API/C++/Z/traits/SelectType.hpp
+++ Z.Kit/v0.1/API/C++/Z/traits/SelectType.hpp
@@ -8,34 +8,54 @@ Released under the terms of the GNU Lesser General Public License v3. */
 #ifndef __Z_traits_SelectType_HPP__
 #define __Z_traits_SelectType_HPP__
 
-#include <Z/macros/repetition.h>
+#include <Z/inspection/language.h>
 
-namespace Zeta {
+#if Z_LANGUAGE_HAS(CPP, VARIADIC_TEMPLATE)
 
-	template <unsigned int,
-		class T00,	  class T01 = void, class T02 = void, class T03 = void,
-		class T04 = void, class T05 = void, class T06 = void, class T07 = void,
-		class T08 = void, class T09 = void, class T10 = void, class T11 = void,
-		class T12 = void, class T13 = void, class T14 = void, class T15 = void,
-		class T16 = void, class T17 = void, class T18 = void, class T19 = void,
-		class T20 = void, class T21 = void, class T22 = void, class T23 = void,
-		class T24 = void, class T25 = void, class T26 = void, class T27 = void,
-		class T28 = void, class T29 = void, class T30 = void, class T31 = void
-	> struct SelectType {};
+	namespace Zeta {
 
-#	define Z_TEMPLATE_SPECIALIZATION(type_count, index)		       \
-	template <Z_ENUMERATE_APPENDING_INDEX(type_count, class T)>	       \
-	struct SelectType<index, Z_ENUMERATE_APPENDING_INDEX(type_count, T)> { \
-		typedef T##index type;					       \
-	};
+		template<unsigned int N, class T, class... types> struct SelectType {
+			typedef typename SelectType<N - 1, types...>::type type;
+		};
 
-#	define Z_TEMPLATE_SPECIALIZATION_GROUP(type_count) \
-	Z_FOR_##type_count##_CALL_WITH_TOKEN_AND_INDEX(Z_TEMPLATE_SPECIALIZATION, type_count,)
+		template<class T, class... types> struct SelectType<0, T, types...> {
+			typedef T type;
+		};
 
-	Z_FOR_32_CALL_WITH_INDEX(Z_TEMPLATE_SPECIALIZATION_GROUP,)
+	}
 
-#	undef Z_TEMPLATE_SPECIALIZATION_GROUP
-#	undef Z_TEMPLATE_SPECIALIZATION
-}
+#else
+
+#	include <Z/macros/repetition.h>
+
+	namespace Zeta {
+
+		template <unsigned int,
+			class T00,	  class T01 = void, class T02 = void, class T03 = void,
+			class T04 = void, class T05 = void, class T06 = void, class T07 = void,
+			class T08 = void, class T09 = void, class T10 = void, class T11 = void,
+			class T12 = void, class T13 = void, class T14 = void, class T15 = void,
+			class T16 = void, class T17 = void, class T18 = void, class T19 = void,
+			class T20 = void, class T21 = void, class T22 = void, class T23 = void,
+			class T24 = void, class T25 = void, class T26 = void, class T27 = void,
+			class T28 = void, class T29 = void, class T30 = void, class T31 = void
+		> struct SelectType {};
+
+#		define Z_TEMPLATE_SPECIALIZATION(type_count, index)		       \
+		template <Z_ENUMERATE_APPENDING_INDEX(type_count, class T)>	       \
+		struct SelectType<index, Z_ENUMERATE_APPENDING_INDEX(type_count, T)> { \
+			typedef T##index type;					       \
+		};
+
+#		define Z_TEMPLATE_SPECIALIZATION_GROUP(type_count) \
+		Z_FOR_##type_count##_CALL_WITH_TOKEN_AND_INDEX(Z_TEMPLATE_SPECIALIZATION, type_count,)
+
+		Z_FOR_32_CALL_WITH_INDEX(Z_TEMPLATE_SPECIALIZATION_GROUP,)
+
+#		undef Z_TEMPLATE_SPECIALIZATION_GROUP
+#		undef Z_TEMPLATE_SPECIALIZATION
+	}
+
+#endif
 
 #endif // __Z_traits_SelectType_HPP__
