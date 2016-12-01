621ad97231098434460fbb90785ca99fb39268ed
redcode <manuel@redcodesoftware.com>
2016-12-01 04:23:26 +0100

    TypeList trait.

--- Z.Kit/v0.1/API/C++/Z/traits/TypeList.hpp
+++ Z.Kit/v0.1/API/C++/Z/traits/TypeList.hpp
@@ -17,8 +17,10 @@ Released under the terms of the GNU Lesser General Public License v3. */
 		template<class... A> struct TypeList {
 			enum {size = sizeof...(A)};
 
-			template<class... T> using prepend = TypeList<T..., A...>;
-			template<class... T> using append  = TypeList<A..., T...>;
+#			if Z_LANGUAGE_HAS(CPP, TEMPLATE_ALIAS)
+				template<class... T> using prepend = TypeList<T..., A...>;
+				template<class... T> using append  = TypeList<A..., T...>;
+#			endif
 		};
 
 	}
