d21b68124a422db474872f57b3d654edfe7bcd56
redcode <manuel@redcodesoftware.com>
2017-03-27 05:08:00 +0200

    CPU integer endianness autodetection.

--- Z.Kit/v0.1/API/C/Z/inspection/compiler/completion.h
+++ Z.Kit/v0.1/API/C/Z/inspection/compiler/completion.h
@@ -1,4 +1,4 @@
-/* Z Kit C API - inspection/modules/completions/compiler.h
+/* Z Kit C API - inspection/compiler/completion.h
  _____  _______________
 /_   /_/  -_/_   _/  _ |
  /____/\___/ /__//___/_| Kit
