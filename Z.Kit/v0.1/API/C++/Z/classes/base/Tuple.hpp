4ea39186771cac6677109ec1b6167f82655bf941
redcode <manuel@redcodesoftware.com>
2017-04-29 22:50:23 +0200

    Tuple class.

--- Z.Kit/v0.1/API/C++/Z/classes/base/Tuple.hpp
+++ Z.Kit/v0.1/API/C++/Z/classes/base/Tuple.hpp
@@ -54,53 +54,53 @@ Released under the terms of the GNU Lesser General Public License v3. */
 	}}}
 
 
-	namespace Zeta {template <class... T> class Tuple;}
+	namespace Zeta {
 
-
-	template <class... T> class Zeta::Tuple
-	: public Zeta::Detail::Tuple::Super<TypeList<T...>, typename TypeListTransform<TypeList<T...>, TypeToParameter>::type>::type {
-		private:
-		typedef TypeList<T...>							      ValueTypeList;
-		typedef typename TypeListTransform<ValueTypeList, TypeToParameter>::type      ParameterTypeList;
-		typedef typename Detail::Tuple::Super<ValueTypeList, ParameterTypeList>::type Super;
-
-		protected:
-		template <UInt I> class At {
+		template <class... T> class Tuple
+		: public Detail::Tuple::Super<TypeList<T...>, typename TypeListTransform<TypeList<T...>, TypeToParameter>::type>::type {
 			private:
-			enum {tail_size = sizeof...(T) - (I + 1)};
+			typedef TypeList<T...>							      ValueTypeList;
+			typedef typename TypeListTransform<ValueTypeList, TypeToParameter>::type      ParameterTypeList;
+			typedef typename Detail::Tuple::Super<ValueTypeList, ParameterTypeList>::type Super;
+
+			protected:
+			template <UInt I> class At {
+				private:
+				enum {tail_size = sizeof...(T) - (I + 1)};
+
+				public:
+				typedef Detail::Tuple::Element<
+					typename TypeListRotateRight<typename TypeListRemoveTail<ValueTypeList,	    tail_size>::type, 1>::type,
+					typename TypeListRotateRight<typename TypeListRemoveTail<ParameterTypeList, tail_size>::type, 1>::type
+				> element;
+
+				typedef typename element::value_type	 value_type;
+				typedef typename element::parameter_type parameter_type;
+			};
 
 			public:
-			typedef Detail::Tuple::Element<
-				typename TypeListRotateRight<typename TypeListRemoveTail<ValueTypeList,	    tail_size>::type, 1>::type,
-				typename TypeListRotateRight<typename TypeListRemoveTail<ParameterTypeList, tail_size>::type, 1>::type
-			> element;
 
-			typedef typename element::value_type	 value_type;
-			typedef typename element::parameter_type parameter_type;
+#			if Z_LANGUAGE_HAS(CPP, INHERITING_CONSTRUCTORS)
+				using Super::Super;
+	#		else
+				Z_CONSTANT_MEMBER(CPP11) Tuple(T... values) : Super(values...) {}
+#			endif
+
+			template <UInt I> Z_INLINE_MEMBER typename At<I>::value_type &at()
+				{return At<I>::element::_value;}
+
+
+			template <UInt I> Z_CONSTANT_MEMBER(CPP11) typename At<I>::value_type get() const
+				{return At<I>::element::_value;}
+
+
+			template <UInt I> Z_INLINE_MEMBER Tuple &set(typename At<I>::parameter_type value)
+				{
+				At<I>::element::_value = value;
+				return *this;
+				}
 		};
-
-		public:
-
-#		if Z_LANGUAGE_HAS(CPP, INHERITING_CONSTRUCTORS)
-			using Super::Super;
-#		else
-			Z_CONSTANT_MEMBER(CPP11) Tuple(T... values) : Super(values...) {}
-#		endif
-
-		template <UInt I> Z_INLINE_MEMBER typename At<I>::value_type &at()
-			{return At<I>::element::_value;}
-
-
-		template <UInt I> Z_CONSTANT_MEMBER(CPP11) typename At<I>::value_type get() const
-			{return At<I>::element::_value;}
-
-
-		template <UInt I> Z_INLINE_MEMBER Tuple &set(typename At<I>::parameter_type value)
-			{
-			At<I>::element::_value = value;
-			return *this;
-			}
-	};
+	}
 
 #endif
 
