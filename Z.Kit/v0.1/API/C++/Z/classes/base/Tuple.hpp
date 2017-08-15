5ef817c41726dbb8cf5c0935a24c60cad70241ab
redcode <manuel@zxe.io>
2017-08-15 15:54:09 +0200

    Coding style.

--- Z.Kit/v0.1/API/C++/Z/classes/base/Tuple.hpp
+++ Z.Kit/v0.1/API/C++/Z/classes/base/Tuple.hpp
@@ -17,15 +17,19 @@ Released under the terms of the GNU Lesser General Public License v3. */
 		template <class value_type_list, class parameter_type_list> class Element;
 		template <class value_type_list, class parameter_type_list> class Super;
 
-		template <class... V, class... P> struct Super<TypeList<V...>, TypeList<P...> > {
+		template <class... V, class... P>
+		struct Super<TypeList<V...>, TypeList<P...> > {
 			typedef Element<
 				typename TypeListRotateRight<TypeList<V...>, 1>::type,
 				typename TypeListRotateRight<TypeList<P...>, 1>::type
 			> type;
 		};
 
-		template <class VN, class... V, class PN, class... P> class Element<TypeList<VN, V...>, TypeList<PN, P...> >
-		: public Super<TypeList<V...>, TypeList<P...> >::type {
+		template <class VN, class... V, class PN, class... P>
+		class Element<TypeList<VN, V...>, TypeList<PN, P...> > : public Super<
+			TypeList<V...>,
+			TypeList<P...>
+		>::type {
 
 			private:
 			typedef typename Super<TypeList<V...>, TypeList<P...> >::type Super;
@@ -48,52 +52,51 @@ Released under the terms of the GNU Lesser General Public License v3. */
 	}}}
 
 
-	namespace Zeta {
+	namespace Zeta {template <class... T> class Tuple : public Detail::Tuple::Super<
+		TypeList<T...>,
+		typename TypeListTransform<TypeList<T...>, TypeToParameter>::type
+	>::type {
 
-		template <class... T> class Tuple
-		: public Detail::Tuple::Super<TypeList<T...>, typename TypeListTransform<TypeList<T...>, TypeToParameter>::type>::type {
+		private:
+		typedef TypeList<T...>							      ValueTypeList;
+		typedef typename TypeListTransform   <ValueTypeList, TypeToParameter  >::type ParameterTypeList;
+		typedef typename Detail::Tuple::Super<ValueTypeList, ParameterTypeList>::type Super;
 
+		protected:
+		template <UInt I> class At {
 			private:
-			typedef TypeList<T...>							      ValueTypeList;
-			typedef typename TypeListTransform<ValueTypeList, TypeToParameter>::type      ParameterTypeList;
-			typedef typename Detail::Tuple::Super<ValueTypeList, ParameterTypeList>::type Super;
-
-			protected:
-			template <UInt I> class At {
-				private:
-				enum {tail_size = sizeof...(T) - (I + 1)};
-
-				public:
-				typedef Detail::Tuple::Element<
-					typename TypeListRotateRight<typename TypeListRemoveTail<ValueTypeList,	    tail_size>::type, 1>::type,
-					typename TypeListRotateRight<typename TypeListRemoveTail<ParameterTypeList, tail_size>::type, 1>::type
-				> Element;
-			};
+			enum {tail_size = sizeof...(T) - (I + 1)};
 
 			public:
-
-#			if Z_LANGUAGE_HAS(CPP, INHERITING_CONSTRUCTORS)
-				using Super::Super;
-#			else
-				Z_CT_MEMBER(CPP11) Tuple(T... values) : Super(values...) {}
-#			endif
-
-
-			template <UInt I> Z_INLINE_MEMBER typename At<I>::Element::Value &at()
-				{return At<I>::Element::_value;}
-
-
-			template <UInt I> Z_CT_MEMBER(CPP11) typename At<I>::Element::Parameter get() const
-				{return At<I>::Element::_value;}
-
-
-			template <UInt I> Z_INLINE_MEMBER Tuple &set(typename At<I>::Element::Parameter value)
-				{
-				At<I>::Element::_value = value;
-				return *this;
-				}
+			typedef Detail::Tuple::Element<
+				typename TypeListRotateRight<typename TypeListRemoveTail<ValueTypeList,	    tail_size>::type, 1>::type,
+				typename TypeListRotateRight<typename TypeListRemoveTail<ParameterTypeList, tail_size>::type, 1>::type
+			> Element;
 		};
-	}
+
+		public:
+
+#		if Z_LANGUAGE_HAS(CPP, INHERITING_CONSTRUCTORS)
+			using Super::Super;
+#		else
+			Z_CT_MEMBER(CPP11) Tuple(T... values) : Super(values...) {}
+#		endif
+
+
+		template <UInt I> Z_INLINE_MEMBER typename At<I>::Element::Value &at()
+			{return At<I>::Element::_value;}
+
+
+		template <UInt I> Z_CT_MEMBER(CPP11) typename At<I>::Element::Parameter get() const
+			{return At<I>::Element::_value;}
+
+
+		template <UInt I> Z_INLINE_MEMBER Tuple &set(typename At<I>::Element::Parameter value)
+			{
+			At<I>::Element::_value = value;
+			return *this;
+			}
+	};}
 
 #endif
 
