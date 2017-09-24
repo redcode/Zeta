caba7ff9c67187fc0afca04419d0bb4fe3b73436
redcode <manuel@zxe.io>
2017-09-24 15:32:30 +0200

    OpaqueMemberFunctionPointer fix.

--- Z.Kit/v0.1/API/C++/Z/classes/base/OpaqueMemberFunctionPointer.hpp
+++ Z.Kit/v0.1/API/C++/Z/classes/base/OpaqueMemberFunctionPointer.hpp
@@ -9,16 +9,15 @@ Released under the terms of the GNU Lesser General Public License v3. */
 #define __Z_classes_base_OpaqueMemberFunctionPointer_HPP__
 
 #include <Z/traits/Type.hpp>
+#include <Z/traits/filtering.hpp>
 
 
 namespace Zeta {struct OpaqueMemberFunctionPointer {
 	void (NaT::* pointer)();
 
-	template <class T, typename = typename EnableIf<
-		Type<T>::is_member_function_pointer
-	>::type>
+	template <class T, class = typename EnableIf<Type<T>::is_member_function_pointer>::type>
 	Z_CT_MEMBER(CPP11) OpaqueMemberFunctionPointer(T pointer)
-	: pointer(reinterpret_cast<void (NaT::*)(void)>(pointer)) {}
+	: pointer(reinterpret_cast<void (NaT::*)()>(pointer)) {}
 };}
 
 
