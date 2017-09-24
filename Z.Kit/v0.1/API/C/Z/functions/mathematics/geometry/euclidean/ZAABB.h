3000123e2b1151110b8a014c096955669db34ddc
redcode <manuel@zxe.io>
2017-09-24 04:46:26 +0200

    ZAABB fixes.

--- Z.Kit/v0.1/API/C/Z/functions/mathematics/geometry/euclidean/ZAABB.h
+++ Z.Kit/v0.1/API/C/Z/functions/mathematics/geometry/euclidean/ZAABB.h
@@ -25,39 +25,38 @@ Z_INLINE ZBox##Type z_aabb_##type##_box(ZAABB##Type object)					\
 	}											\
 												\
 												\
-Z_INLINE zboolean z_aabb_##type##_contains(ZAABB##Type a, ZAABB##Type b)			\
+Z_INLINE zboolean z_aabb_##type##_contains(ZAABB##Type object, ZAABB##Type other)		\
 	{											\
-	return	b.a.x >= a.a.x && b.a.y >= a.a.y && b.a.z >= a.a.z &&				\
-		b.b.x <= a.b.x && b.b.y <= a.b.y && b.b.z <= a.b.z;				\
+	return	other.a.x != other.b.x	&&							\
+		other.a.y != other.b.y  &&							\
+		other.a.z != other.b.z  &&							\
+		other.a.x >= object.a.x &&							\
+		other.a.y >= object.a.y &&							\
+		other.a.z >= object.a.z &&							\
+		other.b.x <= object.b.x &&							\
+		other.b.y <= object.b.y &&							\
+		other.b.z <= object.b.z;							\
 	}											\
 												\
 												\
 Z_INLINE zboolean z_aabb_##type##_contains_box(ZAABB##Type object, ZBox##Type box)		\
 	{											\
-	return	object.a.x <= box.point.x	       &&					\
-		object.a.y <= box.point.y	       &&					\
-		object.a.z <= box.point.z	       &&					\
-		object.b.x >= box.point.x + box.size.x &&					\
-		object.b.y >= box.point.y + box.size.y &&					\
-		object.b.z >= box.point.z + box.size.z;						\
+	return	box.size.x		 != (z##type)0 &&					\
+		box.size.y		 != (z##type)0 &&					\
+		box.size.z		 != (z##type)0 &&					\
+		box.point.x		 >= object.a.x &&					\
+		box.point.y		 >= object.a.y &&					\
+		box.point.z		 >= object.a.z &&					\
+		box.point.x + box.size.x <= object.b.x &&					\
+		box.point.y + box.size.y <= object.b.y &&					\
+		box.point.z + box.size.z <= object.b.z;						\
 	}											\
 												\
 												\
 Z_INLINE zboolean z_aabb_##type##_contains_point(ZAABB##Type object, Z3D##Type point)		\
 	{											\
-	return	object.a.x <= point.x && point.x <= object.a.x &&				\
-		object.a.y <= point.y && point.y <= object.a.y &&				\
-		object.a.z <= point.z && point.z <= object.a.z;					\
-	}											\
-												\
-												\
-Z_INLINE zboolean z_aabb_##type##_contains_line_segment(					\
-	ZAABB##Type   object,									\
-	Z3DLine##Type segment									\
-)												\
-	{											\
-	return	z_aabb_##type##_contains_point(object, segment.a) &&				\
-		z_aabb_##type##_contains_point(object, segment.b);				\
+	return	point.x >= object.a.x && point.y >= object.a.y && point.z >= object.a.z &&	\
+		point.x <  object.b.x && point.y <  object.b.y && point.z <  object.b.z;	\
 	}											\
 												\
 												\
@@ -73,9 +72,10 @@ Z_INLINE ZAABB##Type z_aabb_##type##_from_vertices(Z3D##Type a, Z3D##Type b)			\
 												\
 Z_INLINE zboolean z_aabb_##type##_intersect(ZAABB##Type a, ZAABB##Type b)			\
 	{											\
-	return	a.a.x < b.b.x && b.a.x < a.b.x &&						\
-		a.a.y < b.b.y && b.a.y < a.b.y &&						\
-		a.a.z < b.b.z && b.a.z < a.b.z;							\
+	return	a.a.x != a.b.x && a.a.y != a.b.y && a.a.z != a.b.z &&				\
+		b.a.x != b.b.x && b.a.y != b.b.y && b.a.z != b.b.z &&				\
+		b.b.x >  a.a.x && b.b.y >  a.a.y && b.b.z >  a.a.z &&				\
+		b.a.x <  a.b.x && b.a.y <  a.b.y && b.a.z <  a.b.z;				\
 	}											\
 												\
 												\
@@ -83,11 +83,11 @@ Z_INLINE ZAABB##Type z_aabb_##type##_intersection(ZAABB##Type a, ZAABB##Type b)	
 	{											\
 	z##type x1, x2, y1, y2, z1, z2;								\
 												\
-	return	(x1 = z_##type##_maximum(a.a.x, b.a.x)) <=					\
+	return	(x1 = z_##type##_maximum(a.a.x, b.a.x)) <					\
 		(x2 = z_##type##_minimum(a.b.x, b.b.x)) &&					\
-		(y1 = z_##type##_maximum(a.a.y, b.a.y))	<=					\
+		(y1 = z_##type##_maximum(a.a.y, b.a.y))	<					\
 		(y2 = z_##type##_minimum(a.b.y, b.b.y)) &&					\
-		(z1 = z_##type##_maximum(a.a.z, b.a.z))	<=					\
+		(z1 = z_##type##_maximum(a.a.z, b.a.z))	<					\
 		(z2 = z_##type##_minimum(a.b.z, b.b.z))						\
 												\
 		? z_aabb_##type(x1, y1, z1, x2, y2, z2)						\
@@ -113,17 +113,16 @@ Z_INLINE z##type z_aabb_##type##_volume(ZAABB##Type object)					\
 	{return z_3d_##type##_inner_product(z_3d_##type##_subtract(object.b, object.a));}
 
 
-#define z_aabb_type_box(		  TYPE) Z_INSERT_##TYPE##_fixed_type(z_aabb_, _box		    )
-#define z_aabb_type_contains(		  TYPE) Z_INSERT_##TYPE##_fixed_type(z_aabb_, _contains		    )
-#define z_aabb_type_contains_box(	  TYPE) Z_INSERT_##TYPE##_fixed_type(z_aabb_, _contains_box	    )
-#define z_aabb_type_contains_line_segment(TYPE) Z_INSERT_##TYPE##_fixed_type(z_aabb_, _contains_line_segment)
-#define z_aabb_type_contains_point(	  TYPE) Z_INSERT_##TYPE##_fixed_type(z_aabb_, _contains_point	    )
-#define z_aabb_type_from_vertices(	  TYPE) Z_INSERT_##TYPE##_fixed_type(z_aabb_, _from_vertices	    )
-#define z_aabb_type_intersect(		  TYPE) Z_INSERT_##TYPE##_fixed_type(z_aabb_, _intersect	    )
-#define z_aabb_type_intersection(	  TYPE) Z_INSERT_##TYPE##_fixed_type(z_aabb_, _intersection	    )
-#define z_aabb_type_size(		  TYPE) Z_INSERT_##TYPE##_fixed_type(z_aabb_, _size		    )
-#define z_aabb_type_union(		  TYPE) Z_INSERT_##TYPE##_fixed_type(z_aabb_, _union		    )
-#define z_aabb_type_volume(		  TYPE) Z_INSERT_##TYPE##_fixed_type(z_aabb_, _volume		    )
+#define z_aabb_type_box(	   TYPE) Z_INSERT_##TYPE##_fixed_type(z_aabb_, _box	      )
+#define z_aabb_type_contains(	   TYPE) Z_INSERT_##TYPE##_fixed_type(z_aabb_, _contains      )
+#define z_aabb_type_contains_box(  TYPE) Z_INSERT_##TYPE##_fixed_type(z_aabb_, _contains_box  )
+#define z_aabb_type_contains_point(TYPE) Z_INSERT_##TYPE##_fixed_type(z_aabb_, _contains_point)
+#define z_aabb_type_from_vertices( TYPE) Z_INSERT_##TYPE##_fixed_type(z_aabb_, _from_vertices )
+#define z_aabb_type_intersect(	   TYPE) Z_INSERT_##TYPE##_fixed_type(z_aabb_, _intersect     )
+#define z_aabb_type_intersection(  TYPE) Z_INSERT_##TYPE##_fixed_type(z_aabb_, _intersection  )
+#define z_aabb_type_size(	   TYPE) Z_INSERT_##TYPE##_fixed_type(z_aabb_, _size	      )
+#define z_aabb_type_union(	   TYPE) Z_INSERT_##TYPE##_fixed_type(z_aabb_, _union	      )
+#define z_aabb_type_volume(	   TYPE) Z_INSERT_##TYPE##_fixed_type(z_aabb_, _volume	      )
 
 
 /* MARK: - Partial implementation for real types */
@@ -143,7 +142,8 @@ Z_INLINE Z3D##Type z_aabb_##type##_absolute_point_to_unit(ZAABB##Type object, Z3
 												\
 Z_INLINE zboolean z_aabb_##type##_contains_sphere(ZAABB##Type object, ZSphere##Type sphere)	\
 	{											\
-	return	sphere.point.x - sphere.radius >= object.a.x &&					\
+	return	sphere.radius != (z##type)0		     &&					\
+		sphere.point.x - sphere.radius >= object.a.x &&					\
 		sphere.point.y - sphere.radius >= object.a.y &&					\
 		sphere.point.z - sphere.radius >= object.a.z &&					\
 		sphere.point.x + sphere.radius <= object.b.x &&					\
@@ -352,7 +352,6 @@ Z_IMPLEMENTATION_AABB_COMMON(Int32, int32)
 #	define z_aabb_box		     z_aabb_type_box		       (REAL)
 #	define z_aabb_contains		     z_aabb_type_contains	       (REAL)
 #	define z_aabb_contains_box	     z_aabb_type_contains_box	       (REAL)
-#	define z_aabb_contains_line_segment  z_aabb_type_contains_line_segment (REAL)
 #	define z_aabb_contains_point	     z_aabb_type_contains_point	       (REAL)
 #	define z_aabb_contains_sphere	     z_aabb_type_contains_sphere       (REAL)
 #	define z_aabb_from_vertices	     z_aabb_type_from_vertices	       (REAL)
