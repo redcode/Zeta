a38d638fbed44dad1fe4fde428823a2a3df2bdc2
redcode <manuel@zxe.io>
2018-02-11 03:12:09 +0100

    Z inspection.

---
+++ Z.Kit/v0.1/API/C/Z/inspection/Z.h
@@ -0,0 +1,13 @@
+/* Z Kit C API - inspection/Z.h
+ _____  _______________
+/_   /_/  -_/_   _/  _ |
+ /____/\___/ /__//___/_| Kit
+Copyright (C) 2006-2018 Manuel Sainz de Baranda y Goñi.
+Released under the terms of the GNU Lesser General Public License v3. */
+
+#ifndef __Z_inspection_Z_H__
+#define __Z_inspection_Z_H__
+
+#define Z_HAS(WHAT) Z_HAS_##WHAT
+
+#endif /* __Z_inspection_Z_H__ */
