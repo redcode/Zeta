f1f392e1e5c7794ea9356875d6cc589abe975992
redcode <manuel@zxe.io>
2018-02-18 20:06:49 +0100

    TernaryType trait.

---
+++ Z.Kit/v0.1/API/C++/Z/traits/TernaryType.hpp
@@ -0,0 +1,20 @@
+/* Z Kit C++ API - traits/TernaryType.hpp
+ _____  _______________
+/_   /_/  -_/_   _/  _ |
+ /____/\___/ /__//___/_| Kit
+Copyright (C) 2006-2018 Manuel Sainz de Baranda y Goñi.
+Released under the terms of the GNU Lesser General Public License v3. */
+
+#ifndef __Z_traits_TernaryType_HPP__
+#define __Z_traits_TernaryType_HPP__
+
+#include <Z/types/base.hpp>
+
+namespace Zeta {
+	template <Boolean condition, class if_true, class if_false> struct TernaryType;
+
+	template <class A, class B> struct TernaryType<true,  A, B> {typedef A type;};
+	template <class A, class B> struct TernaryType<false, A, B> {typedef B type;};
+}
+
+#endif // __Z_traits_TernaryType_HPP__
