16b5fdac2c5f6dae10c111833f53421ca40ce9ea
redcode <manuel@redcodesoftware.com>
2018-04-11 03:59:56 +0200

    Range class improvements.

--- Z.Kit/v0.1/API/C++/Z/classes/base/Range.hpp
+++ Z.Kit/v0.1/API/C++/Z/classes/base/Range.hpp
@@ -16,37 +16,28 @@ Released under the terms of the GNU Lesser General Public License v3. */
 #endif
 
 
-namespace Zeta {template <class T> struct Range {
-
-	typedef typename ZTypeFixedNatural(ZRange, T) Base;
-
-	T index, size;
-
+namespace Zeta {template <class T> struct Range : ZTypeFixedNatural(ZRange, T) {
 
 	Z_INLINE_MEMBER Range() {}
 
-	Z_CT_MEMBER(CPP11) Range(T size)	  : index(0),	  size(size) {}
-	Z_CT_MEMBER(CPP11) Range(T index, T size) : index(index), size(size) {}
+	Z_CT_MEMBER(CPP11) Range(T size)	  : {this->index = 0;	  this->size = size;}
+	Z_CT_MEMBER(CPP11) Range(T index, T size) : {this->index = index; this->size = size;}
 
-	Z_INLINE_MEMBER Range(const Base &range) {(*(Base *)this) = range;}
-
-
-	Z_CT_MEMBER(CPP11) operator Boolean() const {return !!size;}
-	Z_INLINE_MEMBER	   operator Base&  () const {return *((Base *)this);}
+	Z_CT_MEMBER(CPP11) operator Boolean() const {return !!this->size;}
 
 
 	Z_CT_MEMBER(CPP11) Boolean operator ==(const Range &rhs) const
-		{return index == rhs.index && size == rhs.size;}
+		{return this->index == rhs.index && this->size == rhs.size;}
 
 
 	Z_CT_MEMBER(CPP11) Boolean operator !=(const Range &rhs) const
-		{return index != rhs.index || size != rhs.size;}
+		{return this->index != rhs.index || this->size != rhs.size;}
 
 
 	Z_CT_MEMBER(CPP14) Range operator &(const Range &rhs) const
 		{
 		T index = (this->index > rhs.index) ? this->index : rhs.index;
-		T end	= Zeta::minimum<T>(this->index + this->size, rhs.index + rhs.size);
+		T end	= Zeta::minimum<T>(end(), rhs.end());
 
 		return end > index ? Range(index, end - index) : Range(0);
 		}
@@ -55,8 +46,8 @@ namespace Zeta {template <class T> struct Range {
 	Z_CT_MEMBER(CPP14) Range operator |(const Range &rhs) const
 		{
 		T	index = (this->index < rhs.index) ? this->index : rhs.index,
-			a_end = this->index + this->size,
-			b_end = rhs.index + rhs.size;
+			a_end = end(),
+			b_end = rhs.end();
 
 		return Range(index, ((a_end > b_end) ? a_end : b_end) - index);
 		}
@@ -71,12 +62,15 @@ namespace Zeta {template <class T> struct Range {
 #	if defined(Z_USE_NS_RANGE) && Z_LANGUAGE_INCLUDES(OBJECTIVE_CPP)
 
 		Z_CT_MEMBER(CPP11) Range(const NSRange &range)
-		: index(range.location), size(range.length) {}
+			{
+			this->index = range.location;
+			this->size  = range.length;
+			}
 
 
 		Z_CT_MEMBER(CPP14) operator NSRange() const
 			{
-			NSRange result = {NSUInteger(index), NSUInteger(size)};
+			NSRange result = {NSUInteger(this->index), NSUInteger(this->size)};
 			return result;
 			}
 
@@ -84,27 +78,27 @@ namespace Zeta {template <class T> struct Range {
 
 
 	Z_CT_MEMBER(CPP11) Boolean contains(const Range &other) const
-		{return other.index >= index && other.index + other.size <= index + size;}
+		{return other.index >= this->index && other.end() <= end();}
 
 
 	Z_CT_MEMBER(CPP11) Boolean contains(T index) const
-		{return index >= this->index && index < this->index + this->size;}
+		{return index >= this->index && index < end();}
 
 
 	Z_CT_MEMBER(CPP11) T end() const
-		{return index + size;}
+		{return this->index + this->size;}
 
 
 	Z_CT_MEMBER(CPP11) Boolean intersects(const Range &other) const
-		{return index < other.index + other.size && other.index < index + size;}
+		{return	this->index < other.end() && other.index < end();}
 
 
 	Z_CT_MEMBER(CPP11) Boolean is_zero() const
-		{return !index && !size;}
+		{return !this->index && !this->size;}
 
 
 	Z_INLINE_MEMBER void swap(Range &other)
-		{Zeta::swap<Base>((Base *)this, (Base *)&other);}
+		{Zeta::swap<Range>(this, &other);}
 };}
 
 
