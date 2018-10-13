5e9395d89e94211bf9ba10f0a3f15e2dd2bff27f
redcode <manuel@zxe.io>
2018-10-13 10:42:14 +0200

    z_gregorian_year_days_in_month() improvement.

--- API/Z/functions/time/date.h
+++ API/Z/functions/time/date.h
@@ -18,9 +18,9 @@ static Z_INLINE zboolean z_gregorian_year_is_leap(zullong year)
 
 static Z_INLINE zuint8 z_gregorian_year_days_in_month(zullong year, zuint8 month)
 	{
-	if (month == 2) return z_gregorian_year_is_leap(year) ? 29 : 28;
-	if (month == 4 || month == 6 || month == 9 || month == 11) return 30;
-	return 31;
+	return month == 2
+		? (Z_GREGORIAN_YEAR_IS_LEAP(year) ? 29 : 28)
+		: (month == 4 || month == 6 || month == 9 || month == 11 ? 30 : 31);
 	}
 
 
