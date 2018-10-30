b0b33d137d61d7cb27a8aacc1d4946ce9fd52418
redcode <manuel@zxe.io>
2018-10-30 02:03:12 +0100

    LGPLv3 headers.

--- API/Z/formats/snapshot/machine/computer/ZX Spectrum/ZX.h
+++ API/Z/formats/snapshot/machine/computer/ZX Spectrum/ZX.h
@@ -13,7 +13,7 @@ the Free Software Foundation, either version 3 of the License, or
 This library is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+GNU Lesser General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public License
 along with this library.  If not, see <http://www.gnu.org/licenses/>.
