fb82be3247ca204fe250cdc73f1793fe3c78ea2e
redcode <manuel@zxe.io>
2018-11-08 15:52:53 +0100

    Shorter license header.

--- API/Z/classes/mathematics/geometry/euclidean/AABR.hpp
+++ API/Z/classes/mathematics/geometry/euclidean/AABR.hpp
@@ -3,19 +3,7 @@
 /_   /_/  -_/_   _/  _ |
  /____/\___/ /__//___/_| Kit
 Copyright (C) 2006-2018 Manuel Sainz de Baranda y Goñi.
--------------------------------------------------------------------------------
-This library is  free software: you can redistribute it  and/or modify it under
-the terms  of the GNU  Lesser General Public License  as published by  the Free
-Software Foundation, either  version 3 of the License, or  (at your option) any
-later version.
-
-This library is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
-
-You should have received  a copy of the GNU Lesser General Public License along
-with this library. If not, see <http://www.gnu.org/licenses/>.
----------------------------------------------------------------------------- */
+Released under the terms of the GNU Lesser General Public License v3. */
 
 #ifndef _Z_classes_mathematics_geometry_euclidean_AABR_HPP_
 #define _Z_classes_mathematics_geometry_euclidean_AABR_HPP_
@@ -287,7 +275,7 @@ namespace Zeta {template <class T> struct AABR : Line2D<T>, Partials::Rectangle:
 		{
 		Z2D##Type old_size = z_aabr_##type##_size(object);
 
-		object.a.x = object.b.x - (size = z_2d_##type##_fit(size, old_size)).x;	
+		object.a.x = object.b.x - (size = z_2d_##type##_fit(size, old_size)).x;
 		object.b.y = (object.a.y += (old_size.y - size.y) / (z##type)2) + size.y;
 		return object;
 		}
