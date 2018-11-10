79f50d537f478c1d4ff055c344bebc373271a10f
redcode <manuel@zxe.io>
2018-11-10 08:04:50 +0100

    Comments.

--- API/Z/keys/platform.h
+++ API/Z/keys/platform.h
@@ -14,52 +14,53 @@ Released under the terms of the GNU Lesser General Public License v3. */
 
 /* Computers */
 
-#define Z_PLATFORM_AMIGA
-#define Z_PLATFORM_APPLE_II
-#define Z_PLATFORM_COMMODORE_16
-#define Z_PLATFORM_COMMODORE_64
-#define Z_PLATFORM_COMMODORE_128
-#define Z_PLATFORM_CPC
-#define Z_PLATFORM_MSX
-#define Z_PLATFORM_QL
-#define Z_PLATFORM_PET
-#define Z_PLATFORM_ST
-#define Z_PLATFORM_VIC_20
-#define Z_PLATFORM_ZX_SPECTRUM
+#define Z_PLATFORM_AMIGA		/* Commodore */
+#define Z_PLATFORM_APPLE_II		/* Apple Computer */
+#define Z_PLATFORM_COMMODORE_16		/* Commodore */
+#define Z_PLATFORM_COMMODORE_64		/* Commodore */
+#define Z_PLATFORM_COMMODORE_128	/* Commodore */
+#define Z_PLATFORM_CPC			/* Amstrad */
+#define Z_PLATFORM_MSX			/* Microsoft and others */
+#define Z_PLATFORM_QL			/* Sinclair Research */
+#define Z_PLATFORM_PET			/* Commodore */
+#define Z_PLATFORM_ST			/* Atari */
+#define Z_PLATFORM_VIC_20		/* Commodore */
+#define Z_PLATFORM_ZX_SPECTRUM		/* Sinclair Research */
 
 /* Video game consoles */
 
-#define Z_PLATFORM_3DO
-#define Z_PLATFORM_ATARI_2600
-#define Z_PLATFORM_ATARI_5200
-#define Z_PLATFORM_ATARI_7800
-#define Z_PLATFORM_CD_I
-#define Z_PLATFORM_DREAMCAST
-#define Z_PLATFORM_GAME_BOY
-#define Z_PLATFORM_GAME_BOY_ADVANCE
-#define Z_PLATFORM_GAME_GEAR
-#define Z_PLATFORM_JAGUAR
-#define Z_PLATFORM_LYNX
-#define Z_PLATFORM_MASTER_SYSTEM
-#define Z_PLATFORM_NEO_GEO
-#define Z_PLATFORM_NEO_GEO_POCKET
-#define Z_PLATFORM_NES
-#define Z_PLATFORM_NINTENDO_64
-#define Z_PLATFORM_NINTENDO_DS
-#define Z_PLATFORM_NINTENDO_GAME_CUBE
-#define Z_PLATFORM_NINTENDO_SWITCH
-#define Z_PLATFORM_PC_ENGINE
-#define Z_PLATFORM_PLAYSTATION
-#define Z_PLATFORM_PLAYSTATION_2
-#define Z_PLATFORM_PLAYSTATION_3
-#define Z_PLATFORM_PLAYSTATION_4
-#define Z_PLATFORM_SEGA_SATURN
-#define Z_PLATFORM_SNES
-#define Z_PLATFORM_SUPERVISION
-#define Z_PLATFORM_WII
-#define Z_PLATFORM_WII_U
-#define Z_PLATFORM_XBOX
-#define Z_PLATFORM_XBOX_360
-#define Z_PLATFORM_XBOX_ONE
+#define Z_PLATFORM_3DO			/* The 3DO Company */
+#define Z_PLATFORM_ATARI_2600		/* Atari */
+#define Z_PLATFORM_ATARI_5200		/* Atari */
+#define Z_PLATFORM_ATARI_7800		/* Atari */
+#define Z_PLATFORM_CD_I			/* Philips */
+#define Z_PLATFORM_DREAMCAST		/* Sega */
+#define Z_PLATFORM_GAME_BOY		/* Nintendo */
+#define Z_PLATFORM_GAME_BOY_ADVANCE	/* Nintendo */
+#define Z_PLATFORM_GAME_GEAR		/* Sega */
+#define Z_PLATFORM_JAGUAR		/* Atari */
+#define Z_PLATFORM_LYNX			/* Atari */
+#define Z_PLATFORM_MASTER_SYSTEM	/* Sega */
+#define Z_PLATFORM_MEGA_DRIVE		/* Sega */
+#define Z_PLATFORM_NEO_GEO		/* SNK */
+#define Z_PLATFORM_NEO_GEO_POCKET	/* SNK */
+#define Z_PLATFORM_NES			/* Nintendo */
+#define Z_PLATFORM_NINTENDO_64		/* Nintendo */
+#define Z_PLATFORM_NINTENDO_DS		/* Nintendo */
+#define Z_PLATFORM_NINTENDO_GAME_CUBE	/* Nintendo */
+#define Z_PLATFORM_NINTENDO_SWITCH	/* Nintendo */
+#define Z_PLATFORM_PC_ENGINE		/* NEC */
+#define Z_PLATFORM_PLAYSTATION		/* Sony */
+#define Z_PLATFORM_PLAYSTATION_2	/* Sony */
+#define Z_PLATFORM_PLAYSTATION_3	/* Sony */
+#define Z_PLATFORM_PLAYSTATION_4	/* Sony */
+#define Z_PLATFORM_SEGA_SATURN		/* Sega */
+#define Z_PLATFORM_SNES			/* Nintendo */
+#define Z_PLATFORM_SUPERVISION		/* Watara */
+#define Z_PLATFORM_WII			/* Nintendo */
+#define Z_PLATFORM_WII_U		/* Nintendo */
+#define Z_PLATFORM_XBOX			/* Microsoft */
+#define Z_PLATFORM_XBOX_360		/* Microsoft */
+#define Z_PLATFORM_XBOX_ONE		/* Microsoft */
 
 #endif /* _Z_keys_platform_H_ */
