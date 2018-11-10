4a2fc3f036ab3d1f09a6d23660bd792065689494
redcode <manuel@zxe.io>
2018-11-10 06:54:01 +0100

    6502 CPU architecture support.

--- API/Z/inspection/CPU/modules/6502.h
+++ API/Z/inspection/CPU/modules/6502.h
@@ -15,8 +15,7 @@ Released under the terms of the GNU Lesser General Public License v3. */
 #define Z_INSERT_CPUArchitecture( left, right) left##6502##right
 #define Z_INSERT_cpu_architecture(left, right) left##6502##right
 
-#define Z_CPU_HAS_INTEGER_8BIT	TRUE
-#define Z_CPU_HAS_INTEGER_16BIT TRUE
+#define Z_CPU_HAS_INTEGER_8BIT TRUE
 
 #define Z_CPU_INTEGER_ENDIANNESS Z_ENDIANNESS_LITTLE
 #define Z_CPU_INTEGER_FORMAT	 Z_INTEGER_FORMAT_2S_COMPLEMENT
