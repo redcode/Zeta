f5e257599da075bfc66220e10d705b343d433b2e
redcode <manuel@zxe.io>
2018-11-10 05:26:39 +0100

    Coding style.

--- API/Z/inspection/C++.h
+++ API/Z/inspection/C++.h
@@ -14,12 +14,16 @@ Released under the terms of the GNU Lesser General Public License v3. */
 #if !defined(Z_CPP) && defined(__cplusplus)
 #	if __cplusplus >= 201703L
 #		define Z_CPP Z_CPP17
+
 #	elif __cplusplus >= 201402L
 #		define Z_CPP Z_CPP14
+
 #	elif __cplusplus >= 201103L
 #		define Z_CPP Z_CPP11
+
 #	elif __cplusplus >= 199711L
 #		define Z_CPP Z_CPP98
+
 #	else
 #		define Z_CPP Z_CPP85
 #	endif
@@ -28,45 +32,31 @@ Released under the terms of the GNU Lesser General Public License v3. */
 #ifdef Z_CPP
 
 #	if Z_CPP == Z_CPP17
-
 #		include <Z/inspection/C++/modules/C++17.h>
-
 #		define Z_CPP_STRING Z_CPP_STRING_CPP17
 
 #	elif Z_CPP == Z_CPP14
-
 #		include <Z/inspection/C++/modules/C++14.h>
-
 #		define Z_CPP_STRING Z_CPP_STRING_CPP14
 
 #	elif Z_CPP == Z_CPP11
-
 #		include <Z/inspection/C++/modules/C++11.h>
-
 #		define Z_CPP_STRING Z_CPP_STRING_CPP11
 
 #	elif Z_CPP == Z_CPP03
-
 #		include <Z/inspection/C++/modules/C++03.h>
-
 #		define Z_CPP_STRING Z_CPP_STRING_CPP03
 
 #	elif Z_CPP == Z_CPP98
-
 #		include <Z/inspection/C++/modules/C++98.h>
-
 #		define Z_CPP_STRING Z_CPP_STRING_CPP98
 
 #	elif Z_CPP == Z_CPP89
-
 #		include <Z/inspection/C++/modules/C++89.h>
-
 #		define Z_CPP_STRING Z_CPP_STRING_CPP89
 
 #	elif Z_CPP == Z_CPP85
-
 #		include <Z/inspection/C++/modules/C++85.h>
-
 #		define Z_CPP_STRING Z_CPP_STRING_CPP85
 
 #	endif
--- API/Z/inspection/C.h
+++ API/Z/inspection/C.h
@@ -14,61 +14,53 @@ Released under the terms of the GNU Lesser General Public License v3. */
 #ifndef Z_C
 #	if !defined(__STDC__)
 #		define Z_C Z_KR_C
+
 #	elif !defined(__STDC_VERSION__)
 #		define Z_C Z_89
+
 #	elif __STDC_VERSION__ >= 201710L
 #		define Z_C Z_C18
+
 #	elif __STDC_VERSION__ >= 201112L
 #		define Z_C Z_C11
+
 #	elif __STDC_VERSION__ >= 199901L
 #		define Z_C Z_99
+
 #	elif __STDC_VERSION__ >= 199409L
 #		define Z_C Z_C94
+
 #	else
 #		define Z_C Z_C90
 #	endif
 #endif
 
 #if Z_C == Z_C18
-
 #	include <Z/inspection/C/modules/C18.h>
-
 #	define Z_C_STRING Z_C_STRING_C18
 
 #elif Z_C == Z_C11
-
 #	include <Z/inspection/C/modules/C11.h>
-
 #	define Z_C_STRING Z_C_STRING_C11
 
 #elif Z_C == Z_C99
-
 #	include <Z/inspection/C/modules/C99.h>
-
 #	define Z_C_STRING Z_C_STRING_C99
 
 #elif Z_C == Z_C94
-
 #	include <Z/inspection/C/modules/C94.h>
-
 #	define Z_C_STRING Z_C_STRING_C94
 
 #elif Z_C == Z_C90
-
 #	include <Z/inspection/C/modules/C90.h>
-
 #	define Z_C_STRING Z_C_STRING_C90
 
 #elif Z_C == Z_C89
-
 #	include <Z/inspection/C/modules/C89.h>
-
 #	define Z_C_STRING Z_C_STRING_C89
 
 #elif Z_C == Z_KR_C
-
 #	include <Z/inspection/C/modules/KR C.h>
-
 #	define Z_C_STRING Z_C_STRING_KR_C
 
 #endif
