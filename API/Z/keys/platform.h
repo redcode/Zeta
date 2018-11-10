71b2f4bca0d695b19d411e32291609515efe1055
redcode <manuel@zxe.io>
2018-11-10 10:42:42 +0100

    Platform keys.

--- API/Z/keys/platform.h
+++ API/Z/keys/platform.h
@@ -8,63 +8,80 @@ Released under the terms of the GNU Lesser General Public License v3. */
 #ifndef _Z_keys_platform_H_
 #define _Z_keys_platform_H_
 
-#define Z_PLATFORM_UNKNOWN 0
+#define Z_PLATFORM_UNKNOWN		0
 
 /* Calculators */
 
 /* Computers */
 
-#define Z_PLATFORM_AMIGA		/* Commodore */
-#define Z_PLATFORM_APPLE_II		/* Apple Computer */
-#define Z_PLATFORM_COMMODORE_16		/* Commodore */
-#define Z_PLATFORM_COMMODORE_64		/* Commodore */
-#define Z_PLATFORM_COMMODORE_128	/* Commodore */
-#define Z_PLATFORM_CPC			/* Amstrad */
-#define Z_PLATFORM_MSX			/* Microsoft and others */
-#define Z_PLATFORM_QL			/* Sinclair Research */
-#define Z_PLATFORM_PET			/* Commodore */
-#define Z_PLATFORM_ST			/* Atari */
-#define Z_PLATFORM_VIC_20		/* Commodore */
-#define Z_PLATFORM_ZX_SPECTRUM		/* Sinclair Research */
+#define Z_PLATFORM_ABC_80		2001 /* Luxor		     */
+#define Z_PLATFORM_ABC_800		2002 /* Luxor		     */
+#define Z_PLATFORM_ALPHATRONIC_PC	2003 /* Triumph-Adler	     */
+#define Z_PLATFORM_AMIGA		2004 /* Commodore	     */
+#define Z_PLATFORM_APPLE_II		2005 /* Apple Computer	     */
+#define Z_PLATFORM_AQUARIUS		2006 /* Mattel		     */
+#define Z_PLATFORM_COMMODORE_16		2007 /* Commodore	     */
+#define Z_PLATFORM_COMMODORE_64		2008 /* Commodore	     */
+#define Z_PLATFORM_COMMODORE_128	2009 /* Commodore	     */
+#define Z_PLATFORM_CPC			2010 /* Amstrad		     */
+#define Z_PLATFORM_GALAKSIJA		2011 /* Voja Antonić	     */
+#define Z_PLATFORM_JUPITER_ACE		2012 /* Jupiter Cantab	     */
+#define Z_PLATFORM_M5			2013 /* SORD		     */
+#define Z_PLATFORM_MSX			2014 /* Microsoft and others */
+#define Z_PLATFORM_QL			2015 /* Sinclair Research    */
+#define Z_PLATFORM_PET			2016 /* Commodore	     */
+#define Z_PLATFORM_RX_78		2017 /* Bandai		     */
+#define Z_PLATFORM_SORCERER		2018 /* Exidy		     */
+#define Z_PLATFORM_ST			2019 /* Atari		     */
+#define Z_PLATFORM_VIC_20		2020 /* Commodore	     */
+#define Z_PLATFORM_X_07			2021 /* Canon		     */
+#define Z_PLATFORM_X1			2022 /* Sharp		     */
+#define Z_PLATFORM_Z88			2023 /* Cambridge Computer   */
+#define Z_PLATFORM_ZX80			2024 /* Sinclair Research    */
+#define Z_PLATFORM_ZX81			2025 /* Sinclair Research    */
+#define Z_PLATFORM_ZX_SPECTRUM		2026 /* Sinclair Research    */
 
 /* Video game consoles */
 
-#define Z_PLATFORM_3DO			/* The 3DO Company */
-#define Z_PLATFORM_ATARI_2600		/* Atari */
-#define Z_PLATFORM_ATARI_5200		/* Atari */
-#define Z_PLATFORM_ATARI_7800		/* Atari */
-#define Z_PLATFORM_CD_I			/* Philips */
-#define Z_PLATFORM_DREAMCAST		/* Sega */
-#define Z_PLATFORM_GAME_BOY		/* Nintendo */
-#define Z_PLATFORM_GAME_BOY_ADVANCE	/* Nintendo */
-#define Z_PLATFORM_GAME_GEAR		/* Sega */
-#define Z_PLATFORM_JAGUAR		/* Atari */
-#define Z_PLATFORM_LYNX			/* Atari */
-#define Z_PLATFORM_MASTER_SYSTEM	/* Sega */
-#define Z_PLATFORM_MEGA_DRIVE		/* Sega */
-#define Z_PLATFORM_NEO_GEO		/* SNK */
-#define Z_PLATFORM_NEO_GEO_POCKET	/* SNK */
-#define Z_PLATFORM_NES			/* Nintendo */
-#define Z_PLATFORM_NINTENDO_64		/* Nintendo */
-#define Z_PLATFORM_NINTENDO_DS		/* Nintendo */
-#define Z_PLATFORM_NINTENDO_GAME_CUBE	/* Nintendo */
-#define Z_PLATFORM_NINTENDO_SWITCH	/* Nintendo */
-#define Z_PLATFORM_PC_ENGINE		/* NEC */
-#define Z_PLATFORM_PLAYSTATION		/* Sony */
-#define Z_PLATFORM_PLAYSTATION_2	/* Sony */
-#define Z_PLATFORM_PLAYSTATION_3	/* Sony */
-#define Z_PLATFORM_PLAYSTATION_4	/* Sony */
-#define Z_PLATFORM_PLAYSTATION_PORTABLE /* Sony */
-#define Z_PLATFORM_PLAYSTATION_VITA	/* Sony */
-#define Z_PLATFORM_SEGA_SATURN		/* Sega */
-#define Z_PLATFORM_SNES			/* Nintendo */
-#define Z_PLATFORM_SUPERVISION		/* Watara */
-#define Z_PLATFORM_VIRTUAL_BOY		/* Nintendo */
-#define Z_PLATFORM_WII			/* Nintendo */
-#define Z_PLATFORM_WII_U		/* Nintendo */
-#define Z_PLATFORM_WONDER_SWAN		/* Bandai */
-#define Z_PLATFORM_XBOX			/* Microsoft */
-#define Z_PLATFORM_XBOX_360		/* Microsoft */
-#define Z_PLATFORM_XBOX_ONE		/* Microsoft */
+#define Z_PLATFORM_3DO			1001 /* The 3DO Company	  */
+#define Z_PLATFORM_ATARI_2600		1002 /* Atari		  */
+#define Z_PLATFORM_ATARI_5200		1003 /* Atari		  */
+#define Z_PLATFORM_ATARI_7800		1004 /* Atari		  */
+#define Z_PLATFORM_CD_I			1005 /* Philips		  */
+#define Z_PLATFORM_COLECO_VISION	1006 /* Coleco Industries */
+#define Z_PLATFORM_DREAMCAST		1007 /* Sega		  */
+#define Z_PLATFORM_GAME_BOY		1008 /* Nintendo	  */
+#define Z_PLATFORM_GAME_BOY_ADVANCE	1009 /* Nintendo	  */
+#define Z_PLATFORM_GAME_GEAR		1010 /* Sega		  */
+#define Z_PLATFORM_JAGUAR		1011 /* Atari		  */
+#define Z_PLATFORM_LYNX			1012 /* Atari		  */
+#define Z_PLATFORM_MASTER_SYSTEM	1013 /* Sega		  */
+#define Z_PLATFORM_MEGA_DRIVE		1014 /* Sega		  */
+#define Z_PLATFORM_NEO_GEO		1015 /* SNK		  */
+#define Z_PLATFORM_NEO_GEO_POCKET	1016 /* SNK		  */
+#define Z_PLATFORM_NES			1017 /* Nintendo	  */
+#define Z_PLATFORM_NINTENDO_64		1018 /* Nintendo	  */
+#define Z_PLATFORM_NINTENDO_DS		1019 /* Nintendo	  */
+#define Z_PLATFORM_NINTENDO_GAME_CUBE	1020 /* Nintendo	  */
+#define Z_PLATFORM_NINTENDO_SWITCH	1021 /* Nintendo	  */
+#define Z_PLATFORM_PC_ENGINE		1022 /* NEC		  */
+#define Z_PLATFORM_PLAYDIA		1023 /* Bandai		  */
+#define Z_PLATFORM_PLAYSTATION		1024 /* Sony		  */
+#define Z_PLATFORM_PLAYSTATION_2	1025 /* Sony		  */
+#define Z_PLATFORM_PLAYSTATION_3	1026 /* Sony		  */
+#define Z_PLATFORM_PLAYSTATION_4	1027 /* Sony		  */
+#define Z_PLATFORM_PLAYSTATION_PORTABLE 1028 /* Sony		  */
+#define Z_PLATFORM_PLAYSTATION_VITA	1029 /* Sony		  */
+#define Z_PLATFORM_SEGA_SATURN		1030 /* Sega		  */
+#define Z_PLATFORM_SNES			1031 /* Nintendo	  */
+#define Z_PLATFORM_SUPERVISION		1032 /* Watara		  */
+#define Z_PLATFORM_VECTREX		1033 /* MB / GCE	  */
+#define Z_PLATFORM_VIRTUAL_BOY		1034 /* Nintendo	  */
+#define Z_PLATFORM_WII			1035 /* Nintendo	  */
+#define Z_PLATFORM_WII_U		1036 /* Nintendo	  */
+#define Z_PLATFORM_WONDER_SWAN		1037 /* Bandai		  */
+#define Z_PLATFORM_XBOX			1038 /* Microsoft	  */
+#define Z_PLATFORM_XBOX_360		1039 /* Microsoft	  */
+#define Z_PLATFORM_XBOX_ONE		1040 /* Microsoft	  */
 
 #endif /* _Z_keys_platform_H_ */
