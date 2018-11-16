3c352b45c941695fa00f0be1731bc646cd9bc5ed
redcode <manuel@zxe.io>
2018-11-16 18:18:11 +0100

    Coding style.

--- API/Z/macros/date.h
+++ API/Z/macros/date.h
@@ -8,6 +8,7 @@ Released under the terms of the GNU Lesser General Public License v3. */
 #ifndef _Z_macros_date_H_
 #define _Z_macros_date_H_
 
-#define Z_GREGORIAN_YEAR_IS_LEAP(year) !((year) % 100 ? (year) % 4 : (year) % 400)
+#define Z_GREGORIAN_YEAR_IS_LEAP(year) \
+	!((year) % 100 ? (year) % 4 : (year) % 400)
 
 #endif /* _Z_macros_date_H_ */
