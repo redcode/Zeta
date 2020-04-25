b5c169a6069534114b949c84f0284096c9fc0d77
redcode <manuel@zxe.io>
2020-04-25 03:12:26 +0200

    C++11 module.

--- API/Z/inspection/C++/modules/C++11.h
+++ API/Z/inspection/C++/modules/C++11.h
@@ -10,6 +10,7 @@ Released under the terms of the GNU Lesser General Public License v3. */
 
 #include <Z/inspection/C++/modules/C++03.h>				/* Paper	Test macro					       */
 									/* -----------  ------------------------------------------------------ */
+#define Z_CPP_HAS_ANONYMOUS_TYPE_AS_TEMPLATE_ARGUMENT		   TRUE /* N2657							       */
 #define Z_CPP_HAS_C99_PREPROCESSOR				   TRUE /* N1653							       */
 #define Z_CPP_HAS_COPY_LIST_INITIALIZATION			   TRUE /* N2672	__cpp_initializer_lists			>= 200806L (!) */
 #define Z_CPP_HAS_CPP11_UNIVERSAL_CHARACTER_NAME		   TRUE /* N2170							       */
@@ -43,18 +44,17 @@ Released under the terms of the GNU Lesser General Public License v3. */
 #define Z_CPP_HAS_REMOVAL_OF_AUTO_AS_STORAGE_CLASS		   TRUE /* N2546							       */
 #define Z_CPP_HAS_RIGHT_ANGLE_BRACKETS				   TRUE /* N1757							       */
 #define Z_CPP_HAS_RVALUE_REFERENCE				   TRUE /* N2118(v1.0)	__cpp_rvalue_references			>= 200610L (!)
-									   N2844(v2.0)
-									   CWG1138							       */
+									   N2844(v2.0)?
+									   CWG1138?							       */
 #define Z_CPP_HAS_SCOPED_ENUMERATION				   TRUE /* N2347							       */
 #define Z_CPP_HAS_STANDARD_LAYOUT				   TRUE /* N2342							       */
 #define Z_CPP_HAS_STANDARDIZED_ATTRIBUTE_SYNTAX			   TRUE /* N2761	__cpp_attributes			>= 200809L (!) */
 #define Z_CPP_HAS_STATIC_ASSERTION				   TRUE /* N1720	__cpp_static_assert			>= 200410L (!) */
-#define Z_CPP_HAS_THREAD_SAFFE_STATIC_INITIALIZATION		   TRUE /* N2660	__cpp_threadsafe_static_init		>= 200806L (!) */
+#define Z_CPP_HAS_THREAD_SAFE_STATIC_INITIALIZATION		   TRUE /* N2660	__cpp_threadsafe_static_init		>= 200806L (!) */
 #define Z_CPP_HAS_TRAILING_COMMA_ALLOWED_IN_ENUMERATION		   TRUE /*								       */
 #define Z_CPP_HAS_TRAILING_RETURN_TYPE				   TRUE /* N2541							       */
 #define Z_CPP_HAS_TYPE_ALIAS					   TRUE /* N2258	__cpp_alias_templates			>= 200704L (!) */
 #define Z_CPP_HAS_TYPE_ALIAS_TEMPLATE				   TRUE /* N2258	__cpp_alias_templates			>= 200704L (!) */
-#define Z_CPP_HAS_UNNAMED_TYPE_AS_TEMPLATE_ARGUMENT		   TRUE /* N2657							       */
 #define Z_CPP_HAS_UNRESTRICTED_UNION				   TRUE /* N2544							       */
 #define Z_CPP_HAS_VARIADIC_TEMPLATE				   TRUE /* N2242	__cpp_variadic_templates		>= 200704L (!) */
 #define Z_CPP_HAS_ATTRIBUTE_CARRIES_DEPENDENCY			   TRUE /* N2782	__has_cpp_attribute(carries_dependency) >= 200809L (!) */
