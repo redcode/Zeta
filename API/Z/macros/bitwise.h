60c2314144c117b4856354cf4a771effb68ab0c3
redcode <manuel@zxe.io>
2022-05-31 21:20:56 +0200

    Bitwise operations.

--- API/Z/macros/bitwise.h
+++ API/Z/macros/bitwise.h
@@ -16,13 +16,19 @@ Released under the terms of the GNU Lesser General Public License v3. */
 /* MARK: - 8-bit operations */
 
 #define Z_UINT8_REVERSE_IN_4BIT(value) \
-	(((value) << 4) | ((value) >> 4))
+	(Z_CAST(zuint8)(	       \
+	((value) << 4) |	       \
+	((value) >> 4)))
 
 #define Z_UINT8_ROTATE_LEFT(value, rotation) \
-	(((value) << (rotation)) | ((value) >> (8 - (rotation))))
+	(Z_CAST(zuint8)(		     \
+	((value) <<	 (rotation) ) |	     \
+	((value) >> (8 - (rotation)))))
 
 #define Z_UINT8_ROTATE_RIGHT(value, rotation) \
-	(((value) >> (rotation)) | ((value) << (8 - (rotation))))
+	(Z_CAST(zuint8)(		      \
+	((value) >>	 (rotation) ) |	      \
+	((value) << (8 - (rotation)))))
 
 #define Z_UINT8_BIG_ENDIAN    Z_SAME
 #define Z_UINT8_LITTLE_ENDIAN Z_SAME
@@ -30,13 +36,19 @@ Released under the terms of the GNU Lesser General Public License v3. */
 /* MARK: - 16-bit operations */
 
 #define Z_UINT16_REVERSE_IN_8BIT(value) \
-	(((value) << 8) | ((value) >> 8))
+	(Z_CAST(zuint16)(		\
+	((value) << 8) |		\
+	((value) >> 8)))
 
 #define Z_UINT16_ROTATE_LEFT(value, rotation) \
-	(((value) << (rotation)) | ((value) >> (16 - (rotation))))
+	(Z_CAST(zuint16)(		      \
+	((value) <<       (rotation) ) |      \
+	((value) >> (16 - (rotation)))))
 
 #define Z_UINT16_ROTATE_RIGHT(value, rotation) \
-	(((value) >> (rotation)) | ((value) << (16 - (rotation))))
+	(Z_CAST(zuint16)(		       \
+	((value) >>	  (rotation) ) |       \
+	((value) << (16 - (rotation)))))
 
 #if Z_ISA_INTEGRAL_ENDIANNESS == Z_ENDIANNESS_BIG
 #	define Z_UINT16_BIG_ENDIAN    Z_SAME
@@ -49,20 +61,27 @@ Released under the terms of the GNU Lesser General Public License v3. */
 
 /* MARK: - 32-bit operations */
 
-#define Z_UINT32_REVERSE_IN_8BIT(value) (		   \
+#define Z_UINT32_REVERSE_IN_8BIT(value)			   \
+	(Z_CAST(zuint32)(				   \
 	 ((value) << 24)				 | \
 	 ((value) >> 24)				 | \
 	(((value) <<  8) & (Z_CAST(zuint32)(255) << 16)) | \
-	(((value) >>  8) & (Z_CAST(zuint32)(255) << 8)))
+	(((value) >>  8) & (Z_CAST(zuint32)(255) <<  8))))
 
 #define Z_UINT32_REVERSE_IN_16BIT(value) \
-	(((value) << 16) | ((value) >> 16))
+	(Z_CAST(zuint32)(		 \
+	((value) << 16) |		 \
+	((value) >> 16)))
 
 #define Z_UINT32_ROTATE_LEFT(value, rotation) \
-	(((value) << (rotation)) | ((value) >> (32 - (rotation))))
+	(Z_CAST(zuint32)(		      \
+	((value) <<	  (rotation) ) |      \
+	((value) >> (32 - (rotation)))))
 
 #define Z_UINT32_ROTATE_RIGHT(value, rotation) \
-	(((value) >> (rotation)) | ((value) << (32 - (rotation))))
+	(Z_CAST(zuint32)(		       \
+	((value) >>	  (rotation) ) |       \
+	((value) << (32 - (rotation)))))
 
 #if Z_ISA_INTEGRAL_ENDIANNESS == Z_ENDIANNESS_BIG
 #	define Z_UINT32_BIG_ENDIAN    Z_SAME
@@ -77,7 +96,8 @@ Released under the terms of the GNU Lesser General Public License v3. */
 
 #ifdef Z_UINT64
 
-#	define Z_UINT64_REVERSE_IN_8BIT(value) (		   \
+#	define Z_UINT64_REVERSE_IN_8BIT(value)			   \
+		(Z_CAST(zuint64)(				   \
 		 ((value) << 56)				 | \
 		 ((value) >> 56)				 | \
 		(((value) << 40) & (Z_CAST(zuint64)(255) << 48)) | \
@@ -85,22 +105,29 @@ Released under the terms of the GNU Lesser General Public License v3. */
 		(((value) << 24) & (Z_CAST(zuint64)(255) << 40)) | \
 		(((value) >> 24) & (Z_CAST(zuint64)(255) << 16)) | \
 		(((value) <<  8) & (Z_CAST(zuint64)(255) << 32)) | \
-		(((value) >>  8) & (Z_CAST(zuint64)(255) << 24)))
+		(((value) >>  8) & (Z_CAST(zuint64)(255) << 24))))
 
-#	define Z_UINT64_REVERSE_IN_16BIT(value) (		     \
+#	define Z_UINT64_REVERSE_IN_16BIT(value)			     \
+		(Z_CAST(zuint64)(				     \
 		 ((value) << 48)				   | \
 		 ((value) >> 48)				   | \
 		(((value) << 16) & (Z_CAST(zuint64)(65535) << 32)) | \
-		(((value) >> 16) & (Z_CAST(zuint64)(65535) << 16)))
+		(((value) >> 16) & (Z_CAST(zuint64)(65535) << 16))))
 
 #	define Z_UINT64_REVERSE_IN_32BIT(value) \
-		(((value) << 32) | ((value) >> 32))
+		(Z_CAST(zuint64)(		\
+		((value) << 32) |		\
+		((value) >> 32)))
 
 #	define Z_UINT64_ROTATE_LEFT(value, rotation) \
-		(((value) << (rotation)) | ((value) >> (64 - (rotation))))
+		(Z_CAST(zuint64)(		     \
+		((value) <<	  (rotation) ) |     \
+		((value) >> (64 - (rotation)))))
 
 #	define Z_UINT64_ROTATE_RIGHT(value, rotation) \
-		(((value) >> (rotation)) | ((value) << (64 - (rotation))))
+		(Z_CAST(zuint64)(		      \
+		((value) >>	  (rotation) ) |      \
+		((value) << (64 - (rotation)))))
 
 #	if Z_ISA_INTEGRAL_ENDIANNESS == Z_ENDIANNESS_BIG
 #		define Z_UINT64_BIG_ENDIAN    Z_SAME
@@ -117,7 +144,8 @@ Released under the terms of the GNU Lesser General Public License v3. */
 
 #ifdef Z_UINT128
 
-#	define Z_UINT128_REVERSE_IN_8BIT(value) (		      \
+#	define Z_UINT128_REVERSE_IN_8BIT(value)			      \
+		(Z_CAST(zuint128)(				      \
 		 ((value) << 120)				    | \
 		 ((value) >> 120)				    | \
 		(((value) << 104) & (Z_CAST(zuint128)(255) << 112)) | \
@@ -133,9 +161,10 @@ Released under the terms of the GNU Lesser General Public License v3. */
 		(((value) <<  24) & (Z_CAST(zuint128)(255) <<  72)) | \
 		(((value) >>  24) & (Z_CAST(zuint128)(255) <<  48)) | \
 		(((value) <<   8) & (Z_CAST(zuint128)(255) <<  64)) | \
-		(((value) >>   8) & (Z_CAST(zuint128)(255) <<  56)))
+		(((value) >>   8) & (Z_CAST(zuint128)(255) <<  56))))
 
-#	define Z_UINT128_REVERSE_IN_16BIT(value) (		       \
+#	define Z_UINT128_REVERSE_IN_16BIT(value)		       \
+		(Z_CAST(zuint128)(				       \
 		 ((value) << 112)				     | \
 		 ((value) >> 112)				     | \
 		(((value) <<  80) & (Z_CAST(zuint128)(65535) << 96)) | \
@@ -143,22 +172,29 @@ Released under the terms of the GNU Lesser General Public License v3. */
 		(((value) <<  48) & (Z_CAST(zuint128)(65535) << 80)) | \
 		(((value) >>  48) & (Z_CAST(zuint128)(65535) << 32)) | \
 		(((value) <<  16) & (Z_CAST(zuint128)(65535) << 64)) | \
-		(((value) >>  16) & (Z_CAST(zuint128)(65535) << 48)))
+		(((value) >>  16) & (Z_CAST(zuint128)(65535) << 48))))
 
-#	define Z_UINT128_REVERSE_IN_32BIT(value) (			   \
+#	define Z_UINT128_REVERSE_IN_32BIT(value)			   \
+		(Z_CAST(zuint128)(					   \
 		 ((value) << 96)					 | \
 		 ((value) >> 96)					 | \
 		(((value) << 32) & (Z_CAST(zuint128)(4294967295) << 64)) | \
-		(((value) >> 32) & (Z_CAST(zuint128)(4294967295) << 32)))
+		(((value) >> 32) & (Z_CAST(zuint128)(4294967295) << 32))))
 
 #	define Z_UINT128_REVERSE_IN_64BIT(value) \
-		(((value) << 64) | ((value) >> 64))
+		(Z_CAST(zuint128)(		 \
+		((value) << 64) |		 \
+		((value) >> 64)))
 
 #	define Z_UINT128_ROTATE_LEFT(value, rotation) \
-		(((value) << (rotation)) | ((value) >> (128 - (rotation))))
+		(Z_CAST(zuint128)(		      \
+		((value) <<	   (rotation) ) |     \
+		((value) >> (128 - (rotation)))))
 
 #	define Z_UINT128_ROTATE_RIGHT(value, rotation) \
-		(((value) >> (rotation)) | ((value) << (128 - (rotation))))
+		(Z_CAST(zuint128)(		|      \
+		((value) >>	   (rotation) ) |      \
+		((value) << (128 - (rotation)))))
 
 #	if Z_ISA_INTEGRAL_ENDIANNESS == Z_ENDIANNESS_BIG
 #		define Z_UINT128_BIG_ENDIAN    Z_SAME
