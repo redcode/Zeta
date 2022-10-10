128e458966b5fd59b962a5bcfe27c5f92009060e
redcode <manuel@zxe.io>
2022-10-10 06:51:14 +0200

    Language inspection.

---
+++ API/Z/inspection/C/modules/C23.h
@@ -0,0 +1,32 @@
+/* Zeta API - Z/inspection/C/modules/C22.h
+ ______ ____________  ___
+|__   /|  ___|__  __|/   \
+  /  /_|  __|  |  | /  *  \
+ /_____|_____| |__|/__/ \__\
+Copyright (C) 2006-2022 Manuel Sainz de Baranda y Goñi.
+Released under the terms of the GNU Lesser General Public License v3. */
+
+#ifndef Z_inspection_C_modules_C22_H
+#define Z_inspection_C_modules_C22_H
+
+#include <Z/inspection/C/modules/C11.h>
+
+#define Z_LANGUAGE_HAS_C23_STANDARDIZED_ATTRIBUTE_SYNTAX     TRUE
+#define Z_LANGUAGE_HAS_C23_ATTRIBUTE_DEPRECATED		     TRUE
+#define Z_LANGUAGE_HAS_C23_ATTRIBUTE_FALLTHROUGH	     TRUE
+#define Z_LANGUAGE_HAS_C23_ATTRIBUTE_MAYBE_UNUSED	     TRUE
+#define Z_LANGUAGE_HAS_C23_ATTRIBUTE_NODISCARD		     TRUE
+#define Z_LANGUAGE_HAS_C23_LITERAL_BOOL			     TRUE
+#define Z_LANGUAGE_HAS_C23_LITERAL_NULLPTR		     TRUE
+#define Z_LANGUAGE_HAS_C23_OPERATOR_ALIGNOF		     TRUE
+#define Z_LANGUAGE_HAS_C23_OPERATOR_OFFSETOF		     TRUE
+#define Z_LANGUAGE_HAS_C23_PREPROCESSOR_DIRECTIVE_ELIFDEF    TRUE
+#define Z_LANGUAGE_HAS_C23_PREPROCESSOR_DIRECTIVE_ELIFNDEF   TRUE
+#define Z_LANGUAGE_HAS_C23_PREPROCESSOR_DIRECTIVE_EMBED	     TRUE
+#define Z_LANGUAGE_HAS_C23_PREPROCESSOR_DIRECTIVE_WARNING    TRUE
+#define Z_LANGUAGE_HAS_C23_PREPROCESSOR_OPERATOR_HAS_INCLUDE TRUE
+#define Z_LANGUAGE_HAS_C23_SPECIFIER_ALIGNAS		     TRUE
+#define Z_LANGUAGE_HAS_C23_STORAGE_CLASS_THREAD_LOCAL	     TRUE
+#define Z_LANGUAGE_HAS_C23_TYPE_BOOL			     TRUE
+
+#endif /* Z_inspection_C_modules_C22_H */
