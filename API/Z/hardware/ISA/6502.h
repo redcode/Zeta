ce6764ae9fc2a146b1ddc1ac8be9c71324939b41
redcode <manuel@zxe.io>
2023-01-13 05:06:31 +0100

    ISA headers.

--- API/Z/hardware/ISA/6502.h
+++ API/Z/hardware/ISA/6502.h
@@ -1,4 +1,4 @@
-/* Zeta API - Z/hardware/CPU/6502.h
+/* Zeta API - Z/hardware/ISA/6502.h
  ______ ____________  ___
 |__   /|  ___|__  __|/   \
   /  /_|  __|  |  | /  *  \
@@ -6,8 +6,8 @@
 Copyright (C) 2006-2022 Manuel Sainz de Baranda y Goñi.
 Released under the terms of the GNU Lesser General Public License v3. */
 
-#ifndef Z_hardware_CPU_6502_H
-#define Z_hardware_CPU_6502_H
+#ifndef Z_hardware_ISA_6502_H
+#define Z_hardware_ISA_6502_H
 
 #define Z_6502_ADDRESS_NMI_POINTER   0xFFFA
 #define Z_6502_ADDRESS_RESET_POINTER 0XFFFC
@@ -15,4 +15,4 @@ Released under the terms of the GNU Lesser General Public License v3. */
 #define Z_6502_ADDRESS_BRK_POINTER   0xFFFE
 #define Z_6502_ADDRESS_STACK	     0x0100
 
-#endif /* Z_hardware_CPU_6502_H */
+#endif /* Z_hardware_ISA_6502_H */
