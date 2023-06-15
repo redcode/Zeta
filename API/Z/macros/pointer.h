6e2f1f1b44f93f7571d47a839d87fe25bd0569f7
redcode <manuel@zxe.io>
2023-06-15 21:22:52 +0200

    Pointer macros.

--- API/Z/macros/pointer.h
+++ API/Z/macros/pointer.h
@@ -11,11 +11,11 @@ Released under the terms of the GNU Lesser General Public License v3. */
 
 #include <Z/types/integral.h>
 
-#define Z_BIP(type, base, index) \
-	(((type)(base)) + (index))
+#define Z_AT(type, base, offset) \
+	((type *)(((zchar *)(void *)(base)) + (offset)))
 
-#define Z_BOP(type, base, offset) \
-	((type)(((zchar *)(void *)(base)) + (offset)))
+#define Z_AT_CONST(type, base, offset) \
+	((type const *)(((zchar const *)(void const *)(base)) + (offset)))
 
 #define Z_INDEX(type, base, pointer) \
 	((zusize)(((zuintptr)pointer - (zuintptr)base) / sizeof(type)))
