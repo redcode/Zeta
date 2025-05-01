ebe5b6c4c46ce08121d55f16f23627b585b52789
agaxia <agaxia@zxe.io>
2025-05-01 20:11:02 +0200

    Add `<Z/inspection/C++/modules/C++23.h>`.

---
+++ API/Z/inspection/C++/modules/C++23.h
@@ -0,0 +1,14 @@
+/* Zeta API - Z/inspection/C++/modules/C++23.h
+ ______  ______________  ___
+|__   / |  ___|___  ___|/   \
+  /  /__|  __|   |  |  /  -  \
+ /______|_____|  |__| /__/ \__\
+Copyright (C) 2006-2025 Manuel Sainz de Baranda y Goñi.
+Released under the terms of the GNU Lesser General Public License v3. */
+
+#ifndef Z_inspection_CPP_modules_CPP23_H
+#define Z_inspection_CPP_modules_CPP23_H
+
+#include <Z/inspection/C++/modules/C++20.h>
+
+#endif /* Z_inspection_CPP_modules_CPP23_H */
